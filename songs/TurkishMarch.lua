24e8f42673315c41f0ad8833795963d7c3fca30b
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:35:12 +0400

    Add public-domain arrangement TurkishMarch

---
+++ songs/TurkishMarch.lua
@@ -0,0 +1,17 @@
+return {
+ Id="turkish-march",Name="Turkish March",Artist="W. A. Mozart",BPM=126,StepsPerBeat=2,Categories={"TikTok Classics","Classical","Upbeat","Advanced"},
+ Notes=[[
+q w E r | t Y u i | o i u Y | t r E w
+q E t u | [qo] - [wi] - | [Eu] - [rY] - | t - - -
+a s D f | g H j k | l k j H | g f D s
+a D g j | [al] - [sk] - | [Dj] - [fH] - | g - - -
+q q w E | r r t Y | u u i o | [qi] - [wo] -
+a a s D | f f g H | j j k l | [ak] - [sl] -
+Q E T Y | U Y T E | [Q!] - [E#] - | [T%] - - -
+a D f H | j H f D | [aj] [DH] [fj] [Hk] | l - - -
+q w E r | t Y u i | o i u Y | t r E w
+[qt] [wY] [Eu] [ri] | [qo] [wi] [Eu] [rY] | t - - -
+a s D f | g H j k | l k j H | g f D s
+[ag] [sH] [Dj] [fk] | [al] [sk] [Dj] [fH] | g - - -
+ ]],
+}
