7843073db247ecef0b6b3c362205e7c1646b8eb0
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 08:14:43 +0400

    Add original Aurora song NeonHearts

---
+++ songs/NeonHearts.lua
@@ -0,0 +1,18 @@
+return {
+    Id="neon-hearts", Name="Neon Hearts", Artist="Velora", BPM=118, StepsPerBeat=2,
+    Categories={"Original","Upbeat","Synth"},
+    Notes=[[
+a a s d | f g h j | [af] - [sg] - | [dh] [fj] [gk] -
+q q w e | r t y u | [qr] - [wt] - | [ey] [ru] [ti] -
+a s d f | g f d s | [ag] - [sf] - | d - - -
+z x c v | b v c x | [zb] - [xv] - | c - - -
+q e t y | u y t e | [qu] [ey] [tu] [wy] | q - - -
+a d g j | k j g d | [ak] - [dj] - | [gh] - - -
+w e r t | y u i o | [wy] - [eu] - | [ri] [to] - -
+s d f g | h j k l | [sh] [dj] [fk] [gl] | l - - -
+a a s d | f g h j | [af] - [sg] - | [dh] [fj] [gk] -
+q w e r | t r e w | [qt] - [wr] - | e - - -
+z c b m | n b c z | [zm] - [cn] - | b - - -
+a s d f | [ag] [sh] [dj] [fk] | [al] - [sk] - | [dj] - - -
+    ]],
+}
