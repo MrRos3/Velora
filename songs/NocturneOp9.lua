82d35b29f932591bc56bcace371e3109c2343eaa
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:35:16 +0400

    Add public-domain arrangement NocturneOp9

---
+++ songs/NocturneOp9.lua
@@ -0,0 +1,17 @@
+return {
+ Id="nocturne-op9",Name="Nocturne Op. 9 No. 2",Artist="Frédéric Chopin",BPM=84,StepsPerBeat=3,Categories={"TikTok Classics","Classical","Romantic","Advanced"},
+ Notes=[[
+q e t | y t e | [qa] - e | [ts] - -
+w r y | u y r | [ws] - r | [yd] - -
+e t u | i u t | [ed] - t | [uf] - -
+q E t | Y t E | [qF] - E | [tH] - -
+a d f | h f d | [aj] - d | [fh] - -
+s f h | j h f | [sk] - f | [hj] - -
+q e t | y u y | t e q | [qu] - -
+w R y | U y R | [wD] - R | [yF] - -
+[Q!] E t | [W@] R y | [eD] T u | [rF] Y i
+a d f | H j H | f d a | [aj] - -
+q E t | y t E | [qa] - [eD] | [tf] - -
+[qa] e t | [ws] r y | [ed] t u | [qa] - -
+ ]],
+}
