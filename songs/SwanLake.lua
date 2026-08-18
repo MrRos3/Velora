5e2d6176240204b47b7693060fe42769c0128f5f
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:35:15 +0400

    Add public-domain arrangement SwanLake

---
+++ songs/SwanLake.lua
@@ -0,0 +1,17 @@
+return {
+ Id="swan-lake",Name="Swan Lake Theme",Artist="P. I. Tchaikovsky",BPM=92,StepsPerBeat=2,Categories={"TikTok Classics","Classical","Dramatic","Advanced"},
+ Notes=[[
+a - s D | f - D s | a - d f | H - f d
+a s D f | H f D s | [aH] - [sf] - | [DD] - - -
+q - w E | r - E w | q - e r | Y - r e
+q w E r | Y r E w | [qY] - [wr] - | [EE] - - -
+a d f H | j H f d | [aj] - [dH] - | [ff] - - -
+s D f g | H g f D | [sH] - [Dg] - | f - - -
+Q - W E | R - E W | [Q!] - [W@] - | [E#] - - -
+a s D f | H j H f | [ak] - [sH] - | [Df] - - -
+q e r Y | u Y r e | [qu] - [eY] - | [rr] - - -
+a - s D | f - H - | [aj] - [sH] - | [Df] - - -
+z x C v | B v C x | [zB] - [xv] - | C - - -
+a s D f | [aH] - [sf] - | [DD] - [fa] - | a - - -
+ ]],
+}
