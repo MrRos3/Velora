667fad0cadbf7ac43db7145d80e5856c148dd1bb
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 20:10:53 +0400

    Add complete Ode to Joy arrangement

---
+++ songs/OdeToJoy.lua
@@ -0,0 +1,63 @@
+-- Velora 0.7.0 complete arrangement generated from the credited MIDI edition.
+-- Source: https://www.mutopiaproject.org/ftp/BeethovenLv/ode/ode.mid
+-- Edition license: Public Domain
+return {
+    Id="ode-to-joy",
+    Name="Ode to Joy",
+    Artist="L. van Beethoven",
+    BPM=100,
+    StepsPerBeat=12,
+    Complete=true,
+    Source="https://www.mutopiaproject.org/ftp/BeethovenLv/ode/ode.mid",
+    SourceLicense="Public Domain",
+    Categories={"Famous","Classical","Starter","Complete"},
+    Notes=[=[
+[wyoa] - - - | - - - - | - - - - | [wyoa] - - -
+- - - - | - - - - | [wtps] - - - | - - - -
+- - - - | [wrod] - - - | - - - - | - - - -
+[0uod] - - - | - - - - | - - - - | s - - -
+- - [Qyp] - | - - - - | [wyoa] - - - | - - - -
+- - - - | [9yIp] - - - | - - - - | - - - -
+[ryo] - - - | - - - - | - - - - | [ryo] - - -
+- - - - | - - - - | [eyIp] - - - | - - - -
+- - - - | [wyoa] - - - | - - - - | - - - -
+[9yoa] - - - | - - - - | - - - - | - - - -
+- - [9yIp] - | - - - - | [9yIp] - - - | - - - -
+- - - - | - - - - | - - - - | - - - -
+[wyoa] - - - | - - - - | - - - - | [wyoa] - - -
+- - - - | - - - - | [wtps] - - - | - - - -
+- - - - | [wrod] - - - | - - - - | - - - -
+[0uod] - - - | - - - - | - - - - | s - - -
+- - [Qyp] - | - - - - | [wyoa] - - - | - - - -
+- - - - | [9yIp] - - - | - - - - | - - - -
+[ryo] - - - | - - - - | - - - - | [ryo] - - -
+- - - - | - - - - | [eyIp] - - - | - - - -
+- - - - | [wyoa] - - - | - - - - | - - - -
+[9yop] - - - | - - - - | - - - - | [9yI] - - -
+- - o - | - - - - | [5ryo] - - - | - - - -
+- - - - | - - - - | - - - - | - - - -
+[9Qyp] - - - | - - - - | - - - - | [9Qyp] - - -
+- - - - | - - - - | [9wya] - - - | - - - -
+- - - - | [90yo] - - - | - - - - | - - - -
+[9Qyp] - - - | - - - - | - - - - | [9wya] - - -
+- - [es] - | - - - - | [9wya] - - - | - - - -
+- - - - | [90yo] - - - | - - - - | - - - -
+[9Qyp] - - - | - - - - | - - - - | [9Qya] - - -
+- - s - | - - - - | [(QIa] - - - | - - - -
+- - - - | [(rIp] - - - | - - - - | - - - -
+[0ruo] - - - | - - - - | - - - - | [6eTp] - - -
+- - - - | - - - - | [9wy] - - - | - - - -
+- - - - | Q - - - | - - - - | - - - -
+[wya] - - - | - - - - | - - - - | [wyoa] - - -
+- - - - | - - - - | [qips] - - - | - - - -
+- - - - | [qipd] - - - | - - - - | - - - -
+[0oad] - - - | - - - - | - - - - | [os] - - -
+- - [Qyp] - | - - - - | [wyoa] - - - | - - - -
+- - - - | [8tup] - - - | - - - - | - - - -
+[8wuo] - - - | - - - - | - - - - | [7wyo] - - -
+- - - - | - - - - | [6tIp] - - - | - - - -
+- - - - | [5yoa] - - - | - - - - | - - - -
+[9yop] - - - | - - - - | - - - - | [9yI] - - -
+- - o - | - - - - | [5ro]
+]=],
+}
