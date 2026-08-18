2185147a2821fd3c11ea588f8336b0b759b74e45
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 08:14:47 +0400

    Add original Aurora song LastLight

---
+++ songs/LastLight.lua
@@ -0,0 +1,18 @@
+return {
+    Id="last-light", Name="Last Light", Artist="Velora", BPM=76, StepsPerBeat=2,
+    Categories={"Original","Cinematic","Slow"},
+    Notes=[[
+q - - e | t - - y | [qu] - - [ey] | - - - -
+a - - d | f - - h | [aj] - - [dh] | - - - -
+q w e t | y - t e | [qy] - [wt] - | e - - -
+z - - c | v - - b | [zn] - - [cv] | - - - -
+w - e r | t - y - | [wi] - [er] - | t - - -
+s - d f | g - h - | [sk] - [df] - | g - - -
+q e t y | u - y t | [qi] - [ey] - | t - - -
+a d f h | j - h f | [ak] - [dh] - | f - - -
+z x c v | b - v c | [zm] - [xn] - | c - - -
+q - e t | y - u - | [qo] - [eu] - | [ty] - - -
+a - s d | f - g h | [ak] - [sg] - | [dh] - - -
+q - - e | t - - y | [qu] - [ey] - | q - - -
+    ]],
+}
