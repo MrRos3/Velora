a3f3750610b06efee9ac151ce981c2cd55859578
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:35:10 +0400

    Add public-domain arrangement FurElise

---
+++ songs/FurElise.lua
@@ -0,0 +1,17 @@
+return {
+ Id="fur-elise",Name="Für Elise",Artist="L. van Beethoven",BPM=112,StepsPerBeat=2,Categories={"TikTok Classics","Classical","Advanced"},
+ Notes=[[
+f D f D f a d s | p - a - | d - f - | a - p -
+f D f D f a d s | p - a - | s - d - | p - - -
+a s d f | g - h - | [ad] - [Df] - | [fg] - - -
+f D f D f a d s | p - a - | d - f - | a - p -
+f D f D f a d s | p - a - | s - d - | p - - -
+[ap] - [ds] - | [fa] - [gD] - | f D f a | d s p -
+q E q E q t e w | [qE] - [wt] - | y T y T | y o t r
+e R e R e y t r | [ey] - [tr] - | q - - -
+f D f D f a d s | p - a - | d - f - | a - p -
+f D f D f a d s | p - a - | s - d - | p - - -
+a s d f | g h j H | [adF] - [Dfg] - | [fh] - - -
+f D f D f a d s | [pa] - [ad] - | [sf] - [dp] - | a - - -
+ ]],
+}
