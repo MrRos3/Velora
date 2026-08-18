0f5b951ab5c134f2f6ff524f11cc45ffc1377c93
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:59:47 +0400

    Replace Prelude in C Major with verified MIDI-derived arrangement

--- songs/BachPrelude.lua
+++ songs/BachPrelude.lua
@@ -1,17 +1,115 @@
+-- Velora 0.6.0 arrangement generated from a public-domain Mutopia MIDI edition.
+-- Source: https://www.mutopiaproject.org/ftp/BachJS/BWV846/wtk1-prelude1/wtk1-prelude1.mid
 return {
- Id="bach-prelude",Name="Prelude in C Major",Artist="J. S. Bach",BPM=96,StepsPerBeat=4,Categories={"TikTok Classics","Classical","Study","Advanced"},
- Notes=[[
-q e t u | e t u o | q e t u | e t u o
-w r y i | r y i p | w r y i | r y i p
-e t u o | t u o [ | e t u o | t u o [
-q E t u | E t u O | q E t u | E t u O
-a d f h | d f h k | a d f h | d f h k
-s f g j | f g j l | s f g j | f g j l
-d g h k | g h k ; | d g h k | g h k ;
-a D f h | D f h K | a D f h | D f h K
-Q E T U | E T U O | Q E T U | E T U O
-[Q!] E T U | [W@] R Y I | [eD] T U O | [rF] Y I P
-a d f h | s f g j | d g h k | f h j l
-q e t u | w r y i | e t u o | [qetu] - - -
- ]],
+    Id="bach-prelude",
+    Name="Prelude in C Major",
+    Artist="J. S. Bach",
+    BPM=96,
+    StepsPerBeat=12,
+    Categories={"TikTok Classics","Classical","Study","Advanced"},
+    Notes=[=[
+t - - u | - - o - | - s - - | f - - o
+- - s - | - f - - | t - - u | - - o -
+- s - - | f - - o | - - s - | - f - -
+t - - y | - - p - | - d - - | g - - p
+- - d - | - g - - | t - - y | - - p -
+- d - - | g - - p | - - d - | - g - -
+r - - y | - - o - | - d - - | g - - o
+- - d - | - g - - | r - - y | - - o -
+- d - - | g - - o | - - d - | - g - -
+t - - u | - - o - | - s - - | f - - o
+- - s - | - f - - | t - - u | - - o -
+- s - - | f - - o | - - s - | - f - -
+t - - u | - - p - | - f - - | j - - p
+- - f - | - j - - | t - - u | - - p -
+- f - - | j - - p | - - f - | - j - -
+t - - y | - - I - | - p - - | d - - I
+- - p - | - d - - | t - - y | - - I -
+- p - - | d - - I | - - p - | - d - -
+r - - y | - - o - | - d - - | h - - o
+- - d - | - h - - | r - - y | - - o -
+- d - - | h - - o | - - d - | - h - -
+r - - t | - - u - | - o - - | s - - u
+- - o - | - s - - | r - - t | - - u -
+- o - - | s - - u | - - o - | - s - -
+e - - t | - - u - | - o - - | s - - u
+- - o - | - s - - | e - - t | - - u -
+- o - - | s - - u | - - o - | - s - -
+9 - - e | - - y - | - I - - | s - - y
+- - I - | - s - - | 9 - - e | - - y -
+- I - - | s - - y | - - I - | - s - -
+w - - r | - - y - | - o - - | a - - y
+- - o - | - a - - | w - - r | - - y -
+- o - - | a - - y | - - o - | - a - -
+w - - E | - - u - | - o - - | S - - u
+- - o - | - S - - | w - - E | - - u -
+- o - - | S - - u | - - o - | - S - -
+q - - e | - - y - | - p - - | d - - y
+- - p - | - d - - | q - - e | - - y -
+- p - - | d - - y | - - p - | - d - -
+q - - W | - - y - | - i - - | a - - y
+- - i - | - a - - | q - - W | - - y -
+- i - - | a - - y | - - i - | - a - -
+0 - - w | - - t - | - o - - | s - - t
+- - o - | - s - - | 0 - - w | - - t -
+- o - - | s - - t | - - o - | - s - -
+0 - - q | - - e - | - t - - | i - - e
+- - t - | - i - - | 0 - - q | - - e -
+- t - - | i - - e | - - t - | - i - -
+9 - - q | - - e - | - t - - | i - - e
+- - t - | - i - - | 9 - - q | - - e -
+- t - - | i - - e | - - t - | - i - -
+5 - - 9 | - - w - | - r - - | i - - w
+- - r - | - i - - | 5 - - 9 | - - w -
+- r - - | i - - w | - - r - | - i - -
+8 - - 0 | - - w - | - t - - | u - - w
+- - t - | - u - - | 8 - - 0 | - - w -
+- t - - | u - - w | - - t - | - u - -
+8 - - w | - - E - | - t - - | u - - E
+- - t - | - u - - | 8 - - w | - - E -
+- t - - | u - - E | - - t - | - u - -
+4 - - q | - - e - | - t - - | u - - e
+- - t - | - u - - | 4 - - q | - - e -
+- t - - | u - - e | - - t - | - u - -
+$ - - 8 | - - e - | - t - - | Y - - e
+- - t - | - Y - - | $ - - 8 | - - e -
+- t - - | Y - - e | - - t - | - Y - -
+% - - q | - - r - | - t - - | y - - r
+- - t - | - y - - | % - - q | - - r -
+- t - - | y - - r | - - t - | - y - -
+5 - - q | - - w - | - r - - | y - - w
+- - r - | - y - - | 5 - - q | - - w -
+- r - - | y - - w | - - r - | - y - -
+5 - - 0 | - - w - | - t - - | u - - w
+- - t - | - u - - | 5 - - 0 | - - w -
+- t - - | u - - w | - - t - | - u - -
+5 - - 9 | - - w - | - t - - | i - - w
+- - t - | - i - - | 5 - - 9 | - - w -
+- t - - | i - - w | - - t - | - i - -
+5 - - 9 | - - w - | - r - - | i - - w
+- - r - | - i - - | 5 - - 9 | - - w -
+- r - - | i - - w | - - r - | - i - -
+5 - - ( | - - e - | - t - - | I - - e
+- - t - | - I - - | 5 - - ( | - - e -
+- t - - | I - - e | - - t - | - I - -
+5 - - 0 | - - w - | - t - - | o - - w
+- - t - | - o - - | 5 - - 0 | - - w -
+- t - - | o - - w | - - t - | - o - -
+5 - - 9 | - - w - | - t - - | i - - w
+- - t - | - i - - | 5 - - 9 | - - w -
+- t - - | i - - w | - - t - | - i - -
+5 - - 9 | - - w - | - r - - | i - - w
+- - r - | - i - - | 5 - - 9 | - - w -
+- r - - | i - - w | - - r - | - i - -
+1 - - 8 | - - w - | - E - - | u - - w
+- - E - | - u - - | 1 - - 8 | - - w -
+- E - - | u - - w | - - E - | - u - -
+1 - - 8 | - - q - | - e - - | t - - i
+- - t - | - e - - | t - - e | - - q -
+- e - - | q - - 9 | - - q - | - 9 - -
+1 - - 7 | - - o - | - a - - | d - - g
+- - d - | - a - - | d - - a | - - o -
+- a - - | y - - i | - - u - | - y - -
+[18uos]
+]=],
 }
