d6c657168040da94899a042260345c07f2917ae3
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:35:11 +0400

    Add public-domain arrangement MoonlightSonata

---
+++ songs/MoonlightSonata.lua
@@ -0,0 +1,17 @@
+return {
+ Id="moonlight-sonata",Name="Moonlight Sonata",Artist="L. van Beethoven",BPM=74,StepsPerBeat=3,Categories={"TikTok Classics","Classical","Dark","Advanced"},
+ Notes=[[
+[qa] e t | [qa] e t | [ws] r y | [ws] r y
+[ed] t u | [ed] t u | [wf] r y | [wf] r y
+[qa] E t | [qa] E t | [ws] R y | [ws] R y
+[ed] T u | [ed] T u | [qg] e t | [qg] e t
+[ah] d f | [ah] d f | [sj] f g | [sj] f g
+[dk] g h | [dk] g h | [sl] f g | [sl] f g
+[qa] e T | [qa] e T | [wS] r Y | [wS] r Y
+[eD] t U | [eD] t U | [qF] e T | [qF] e T
+[Q!] E t | [Q!] E t | [W@] R y | [W@] R y
+[eD] t u | [eD] t u | [wf] r y | [wf] r y
+[qa] e t | [qa] e t | [ws] r y | [ws] r y
+[ed] T u | [ed] T u | [qa] e t | [qa] - -
+ ]],
+}
