e4666731182681c143ec12f37ca4c66ce60692ec
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:35:18 +0400

    Add public-domain arrangement BachPrelude

---
+++ songs/BachPrelude.lua
@@ -0,0 +1,17 @@
+return {
+ Id="bach-prelude",Name="Prelude in C Major",Artist="J. S. Bach",BPM=96,StepsPerBeat=4,Categories={"TikTok Classics","Classical","Study","Advanced"},
+ Notes=[[
+q e t u | e t u o | q e t u | e t u o
+w r y i | r y i p | w r y i | r y i p
+e t u o | t u o [ | e t u o | t u o [
+q E t u | E t u O | q E t u | E t u O
+a d f h | d f h k | a d f h | d f h k
+s f g j | f g j l | s f g j | f g j l
+d g h k | g h k ; | d g h k | g h k ;
+a D f h | D f h K | a D f h | D f h K
+Q E T U | E T U O | Q E T U | E T U O
+[Q!] E T U | [W@] R Y I | [eD] T U O | [rF] Y I P
+a d f h | s f g j | d g h k | f h j l
+q e t u | w r y i | e t u o | [qetu] - - -
+ ]],
+}
