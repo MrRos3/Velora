0223cdfe20c20e148d73040ccbd32d1d57549144
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:59:43 +0400

    Replace Turkish March with verified MIDI-derived arrangement

--- songs/TurkishMarch.lua
+++ songs/TurkishMarch.lua
@@ -1,17 +1,132 @@
+-- Velora 0.6.0 arrangement generated from a public-domain Mutopia MIDI edition.
+-- Source: https://www.mutopiaproject.org/ftp/MozartWA/KV331/KV331_3_RondoAllaTurca/KV331_3_RondoAllaTurca.mid
 return {
- Id="turkish-march",Name="Turkish March",Artist="W. A. Mozart",BPM=126,StepsPerBeat=2,Categories={"TikTok Classics","Classical","Upbeat","Advanced"},
- Notes=[[
-q w E r | t Y u i | o i u Y | t r E w
-q E t u | [qo] - [wi] - | [Eu] - [rY] - | t - - -
-a s D f | g H j k | l k j H | g f D s
-a D g j | [al] - [sk] - | [Dj] - [fH] - | g - - -
-q q w E | r r t Y | u u i o | [qi] - [wo] -
-a a s D | f f g H | j j k l | [ak] - [sl] -
-Q E T Y | U Y T E | [Q!] - [E#] - | [T%] - - -
-a D f H | j H f D | [aj] [DH] [fj] [Hk] | l - - -
-q w E r | t Y u i | o i u Y | t r E w
-[qt] [wY] [Eu] [ri] | [qo] [wi] [Eu] [rY] | t - - -
-a s D f | g H j k | l k j H | g f D s
-[ag] [sH] [Dj] [fk] | [al] [sk] [Dj] [fH] | g - - -
- ]],
+    Id="turkish-march",
+    Name="Turkish March",
+    Artist="W. A. Mozart",
+    BPM=126,
+    StepsPerBeat=12,
+    Categories={"TikTok Classics","Classical","Upbeat","Advanced"},
+    Notes=[=[
+a - - p | - - O - | - p - - | [es] - - -
+- - [tu] - | - - - - | [tud] - - s | - - [tua] -
+- s - - | [ef] - - - | - - [tu] - | - - - -
+[tug] - - f | - - [tuD] - | - f - - | [ek] - - j
+- - [tuH] - | - j - - | [ek] - - j | - - [tuH] -
+- j - - | [el] - - - | - - [tu] - | - - - -
+[tuj] - - - | - - [tul] - | - - - h | [0jk] - - -
+- - [ruGj] - | - - - - | [rufh] - - - | - - [ruGj] -
+- - - h | [0jk] - - - | - - [ruGj] - | - - - -
+[rufh] - - - | - - [ruGj] - | - - - h | [0jk] - - -
+- - [ruGj] - | - - - - | [7fh] - - - | - - [rDG] -
+- - - - | [0f] - - - | - - - - | - - - -
+[sf] - - - | - - [dg] - | - - - - | [8fh] - - -
+- - [tfh] - | - - - - | [0j] - - h | - - [ug] -
+- f - - | [wad] - - - | - - o - | - - - -
+[sf] - - - | - - [dg] - | - - - - | [8fh] - - -
+- - [tfh] - | - - - - | [0j] - - h | - - [ug] -
+- f - - | [wad] - - - | - - - - | - - - -
+[ps] - - - | - - [ad] - | - - - - | [6sf] - - -
+- - [esf] - | - - - - | [8g] - - f | - - [td] -
+- s - - | [0Oa] - - - | - - u - | - - - -
+[ps] - - - | - - [ad] - | - - - - | [6sf] - - -
+- - [esf] - | - - - - | [8g] - - f | - - [td] -
+- s - - | [0Oa] - - - | - - - - | - - - -
+a - - p | - - O - | - p - - | [es] - - -
+- - [tu] - | - - - - | [tud] - - s | - - [tua] -
+- s - - | [ef] - - - | - - [tu] - | - - - -
+[tug] - - f | - - [tuD] - | - f - - | [ek] - - j
+- - [tuH] - | - j - - | [ek] - - j | - - [tuH] -
+- j - - | [ql] - - - | - - [eY] - | - - - -
+[eYj] - - - | - - [eYk] - | - - - - | [0l] - - -
+- - [euk] - | - - - - | [9j] - - - | - - [qrH] -
+- - - - | [8j] - - - | - - [0ef] - | - - - -
+[9g] - - - | - - [qrd] - | - - - - | [0es] - - -
+- - [0e] - | - - - - | [0Wa] - - - | - - [0W] -
+- p a - | [6ep] - - - | - - - - | - - - -
+[pj] - - - | - - [ak] - | - - - [6*] | [0eSL] - - -
+- - e - | - - - - | [epj] - - - | - - [eak] -
+- - - [6*] | [0eSL] - - - | - - [eak] - | - - - -
+[epj] - - - | - - [eOH] - | - - - [2$] | [69IG] - - -
+- - [9OH] - | - - - [@$] | [6(pj] - - - | - - [(ak] -
+- - - [3%] | [70OH] - - - | - - [0uf] - | - - - -
+[0pj] - - - | - - [0ak] - | - - - [6*] | [0eSL] - - -
+- - e - | - - - - | [epj] - - - | - - [eak] -
+- - - [6*] | [0eSL] - - - | - - [eak] - | - - - -
+[epj] - - - | - - [eOH] - | - - - [2$] | [69IG] - - -
+- - [9ak] - | - - - [3%] | [70OH] - - - | - - [0uf] -
+- - - - | [6pj] - - - | - - - - | - - - -
+L - - z | - - L - | - k - - | [Qj] - - k
+- - [eTj] - | - H - - | [eTG] - - j | - - [eTH] -
+- G - - | [Wg] - - G | - - [rTH] - | - g - -
+[rTS] - - D | - - [rTg] - | - S - - | [QG] - - g
+- - [eTG] - | - H - - | [eTj] - - H | - - [eTj] -
+- k - - | [qL] - - l | - - [WTL] - | - l - -
+[WTL] - - z | - - [WTL] - | - k - - | [Qj] - - k
+- - [eTj] - | - H - - | [eTG] - - j | - - [eTH] -
+- G - - | [Wf] - - G | - - [TuH] - | - f - -
+[TuS] - - D | - - [Tuf] - | - S - - | [WD] - - f
+- - [YIG] - | - D - - | [YIs] - - S | - - [YID] -
+- s - - | [TuS] - - - | - - - - | - - - -
+f - - d | - - S - | - a - - | [ep] - - a
+- - [TuS] - | - d - - | [Tuf] - - G | - - [TuH] -
+- j - - | [rj] - - H | - - [yuG] - | - f - -
+[Wf] - - d | - - [yuS] - | - a - - | [ep] - - a
+- - [TuS] - | - d - - | [Tuf] - - G | - - [TuH] -
+- j - - | [0J] - - - | - - [Wyk] - | - - - -
+[Wyf] - - d | - - [WyS] - | - a - - | [ep] - - a
+- - [TuS] - | - d - - | [Tuf] - - G | - - [TuH] -
+- j - - | [rj] - - H | - - [yuG] - | - f - -
+[Wf] - - d | - - [yuS] - | - a - - | [eS] - - f
+- - [Qp] - | - S - - | [9a] - - d | - - [0O] -
+- a - - | [6p] - - - | - - e - | - - - -
+L - - z | - - L - | - k - - | [Qj] - - k
+- - [eTj] - | - H - - | [eTG] - - j | - - [eTH] -
+- G - - | [Wg] - - G | - - [rTH] - | - g - -
+[rTS] - - D | - - [rTg] - | - S - - | [QG] - - g
+- - [eTG] - | - H - - | [eTj] - - H | - - [eTj] -
+- k - - | [*L] - - l | - - [WTL] - | - l - -
+[wTL] - - l | - - [QTL] - | - J - - | [7z] - - L
+- - [Qrz] - | - L - - | [Qrz] - - L | - - [Qrz] -
+- L - - | [7z] - - L | - - [Wrk] - | - j - -
+[WrH] - - j | - - [Wrk] - | - H - - | [*j] - - k
+- - [QeL] - | - G - - | [*g] - - G | - - [WrH] -
+- g - - | [QeG] - - - | - - - - | - - - -
+[pj] - - - | - - [ak] - | - - - [6*] | [0eSL] - - -
+- - e - | - - - - | [epj] - - - | - - [eak] -
+- - - [6*] | [0eSL] - - - | - - [eak] - | - - - -
+[epj] - - - | - - [eOH] - | - - - [2$] | [69IG] - - -
+- - [9OH] - | - - - [@$] | [6(pj] - - - | - - [(ak] -
+- - - [3%] | [70OH] - - - | - - [0uf] - | - - - -
+[0pj] - - - | - - [0ak] - | - - - [6*] | [0eSL] - - -
+- - e - | - - - - | [epj] - - - | - - [eak] -
+- - - [6*] | [0eSL] - - - | - - [eak] - | - - - -
+[epj] - - - | - - [eOH] - | - - - [2$] | [69IG] - - -
+- - [9ak] - | - - - [3%] | [70OH] - - - | - - [0uf] -
+- - - - | [6pj] - - - | - - - - | - - - -
+a - - p | - - O - | - p - - | [es] - - -
+- - [tu] - | - - - - | [tud] - - s | - - [tua] -
+- s - - | [ef] - - - | - - [tu] - | - - - -
+[tug] - - f | - - [tuD] - | - f - - | [ek] - - j
+- - [tuH] - | - j - - | [ek] - - j | - - [tuH] -
+- j - - | [el] - - - | - - [tu] - | - - - -
+[tuj] - - - | - - [tul] - | - - - h | [0jk] - - -
+- - [ruGj] - | - - - - | [rufh] - - - | - - [ruGj] -
+- - - h | [0jk] - - - | - - [ruGj] - | - - - -
+[rufh] - - - | - - [ruGj] - | - - - h | [0jk] - - -
+- - [ruGj] - | - - - - | [7fh] - - - | - - [rDG] -
+- - - - | [0f] - - - | - - - - | - - - -
+[sf] - - - | - - [dg] - | - - - - | [8fh] - - -
+- - [tfh] - | - - - - | [0j] - - h | - - [ug] -
+- f - - | [wad] - - - | - - o - | - - - -
+[sf] - - - | - - [dg] - | - - - - | [8fh] - - -
+- - [tfh] - | - - - - | [0j] - - h | - - [ug] -
+- f - - | [wad] - - - | - - - - | - - - -
+[ps] - - - | - - [ad] - | - - - - | [6sf] - - -
+- - [esf] - | - - - - | [8g] - - f | - - [td] -
+- s - - | [0Oa] - - - | - - u - | - - - -
+[ps] - - - | - - [ad] - | - - - - | [6sf] - - -
+- - [esf] - | - - - - | [8g] - - f | - - [td] -
+- s - - | [0Oa]
+]=],
 }
