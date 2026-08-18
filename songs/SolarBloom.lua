8dd7b45b00a71d5c1b3607c5084a98afdc8fa5e0
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 08:14:45 +0400

    Add original Aurora song SolarBloom

---
+++ songs/SolarBloom.lua
@@ -0,0 +1,18 @@
+return {
+    Id="solar-bloom", Name="Solar Bloom", Artist="Velora", BPM=110, StepsPerBeat=2,
+    Categories={"Original","Bright","Upbeat"},
+    Notes=[[
+q e t y | u y t e | [qu] - [ey] - | [tu] - - -
+a d f h | j h f d | [aj] - [dh] - | [fj] - - -
+w r t u | i u t r | [wi] - [ru] - | [ty] - - -
+s f g j | k j g f | [sk] - [fj] - | [gh] - - -
+q w e r | t y u i | [qt] [wy] [eu] [ri] | i - - -
+a s d f | g h j k | [ag] [sh] [dj] [fk] | k - - -
+z c v n | m n v c | [zm] - [cn] - | [vb] - - -
+q e t u | y t e q | [qu] [ey] [tt] [wu] | q - - -
+a d g j | h g d a | [aj] - [dg] - | [gh] - - -
+w e t y | u y t e | [wu] - [ey] - | [ty] - - -
+s d f h | j h f d | [sj] - [dh] - | f - - -
+q e t y | [qu] [ey] [tu] [yi] | [qo] - - -
+    ]],
+}
