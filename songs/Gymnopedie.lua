81ac28f405f927546b985a8d1b4e4f2379fde763
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:35:17 +0400

    Add public-domain arrangement Gymnopedie

---
+++ songs/Gymnopedie.lua
@@ -0,0 +1,17 @@
+return {
+ Id="gymnopedie",Name="Gymnopédie No. 1",Artist="Erik Satie",BPM=72,StepsPerBeat=3,Categories={"TikTok Classics","Classical","Calm","Advanced"},
+ Notes=[[
+[qa] e t | [ws] r y | [qa] e t | [ws] r y
+[ed] t u | [rf] y i | [ed] t u | [rf] y i
+[qa] E t | [wS] R y | [qa] E t | [wS] R y
+[ed] T u | [rF] Y i | [ed] T u | [rF] Y i
+[ah] d f | [sj] f g | [ah] d f | [sj] f g
+[dk] g h | [fl] h j | [dk] g h | [fl] h j
+[qa] e t | [ws] r y | [ed] t u | [rf] y i
+[qa] E t | [wS] R y | [eD] T u | [rF] Y i
+[Q!] E t | [W@] R y | [eD] T u | [rF] Y i
+[ah] d f | [sj] f g | [dk] g h | [fl] h j
+[qa] e t | [ws] r y | [ed] t u | [rf] y i
+[qa] - e | [wt] - r | [qy] - - | q - -
+ ]],
+}
