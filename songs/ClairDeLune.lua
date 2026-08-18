8d1e0cebe13044b1e85bbde441fcd846c7f7f2e5
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-18 19:35:14 +0400

    Add public-domain arrangement ClairDeLune

---
+++ songs/ClairDeLune.lua
@@ -0,0 +1,17 @@
+return {
+ Id="clair-de-lune",Name="Clair de Lune",Artist="Claude Debussy",BPM=68,StepsPerBeat=3,Categories={"TikTok Classics","Classical","Dreamy","Advanced"},
+ Notes=[[
+q - e | t - y | [qu] - e | [ty] - -
+w - r | y - u | [wi] - r | [yu] - -
+[qa] e t | [ws] r y | [ed] t u | [rf] y i
+[qa] E t | [ws] R y | [eD] T u | [rf] y i
+a - d | f - h | [aj] - d | [fh] - -
+s - f | h - j | [sk] - f | [hj] - -
+q e t | y u y | t e q | [qu] - -
+w r y | u i u | y r w | [wi] - -
+[Q!] E t | [W@] R y | [eD] T u | [rF] Y i
+a d f | h j h | f d a | [aj] - -
+q - e | t - y | [qu] - [ey] | [wt] - -
+[qa] - [eD] | [tf] - [yh] | [qj] - - | q - -
+ ]],
+}
