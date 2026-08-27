99a0d05b4b360727c411a6e1ea3c7821d7b43726
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-27 17:50:12 +0400

    Add Comptine d'un autre ete MIDI arrangement

---
+++ songs/ComptineDUnAutreEte.lua
@@ -0,0 +1,82 @@
+-- Velora 0.10.20 Nova — complete arrangement converted from a MIDI supplied by the repository owner.
+-- Supplied file: comptine-dun-autre-ete-arr-kyle-landry-2021.mid
+-- Supplied file SHA-256: 4DE733F891AB695B9C19DFB96CF3AB0D6599A93D56FD0267B5B0344F94CE93BB
+-- MIDI: 480 PPQ, 185 source tempo events, source time signature(s): 4/4.
+-- Timing normalized to real seconds at 90 BPM / 48 Velora steps per beat, preserving all source tempo changes.
+-- 2 non-drum source tracks retained; simultaneous duplicate pitches are merged.
+-- 88 out-of-range note-on events were folded by octave into Velora's C2-C7 keyboard range.
+-- Arrangement credit in supplied filename: Kyle Landry (2021).
+
+local DATA = [=[
+0:[0u] 23:r 23:[wu] 23:r 23:[0u] 23:r 23:[wu] 23:r 23:[9y] 23:r 23:[wy] 23:r 23:[9y] 23:r 23:[wy] 23:r 23:[9y] 23:r 23:[Qy] 23:r 23:[9y] 23:r 23:[Qy] 23:r 23:[9y] 23:e 23:[Qy] 23:e 23:[9y] 23:e 23:[Qy] 23:e
+23:[0u] 23:[ro] 11:I 11:[wuo] 23:[ra] 11:s 11:[0ua] 23:r 23:[wu] 23:r 23:[9y] 23:[rI] 11:o 11:[wyI] 23:[ro] 11:p 11:[9yo] 23:r 23:[wy] 23:r 23:[9y] 23:[rI] 11:u 11:[QyI] 23:[ra] 11:s 11:[9ya] 23:r 23:[Qy] 23:r 23:[9y] 23:[eI]
+11:u 11:[QyI] 23:e 23:[9y] 23:e 23:[Qy] 23:e 23:[0u] 23:[ro] 11:I 11:[wuo] 23:[ra] 11:s 11:[0ua] 23:r 23:[wu] 23:r 23:[9y] 23:[rI] 11:o 11:[wyI] 23:[ro] 11:p 11:[9yo] 23:r 23:[wy] 23:r 23:[9y] 23:[rI] 11:u 11:[QyI] 23:[ra]
+11:s 11:[9ya] 23:r 23:[Qy] 23:r 23:[9y] 23:[eI] 11:u 11:[QyI] 23:e 23:[9y] 23:e 23:[Qy] 23:e 23:[0uf] 23:r 23:[wu] 23:[ra] 23:[0u] 23:r 23:[wu] 23:r 23:[9yd] 23:r 23:[wy] 23:[ra] 23:[9y] 23:r 23:[wy] 23:r 23:[9yG] 23:r
+23:[Qy] 23:[ra] 23:[9y] 23:r 23:[Qy] 23:r 23:[9yG] 23:e 23:[Qy] 23:[ep] 23:[9y] 23:e 23:[Qy] 23:e 23:[0uah] 23:r 23:[wu] 23:[rof] 23:[0u] 23:r 23:[wu] 23:r 23:[9yah] 23:r 23:[wy] 23:[rod] 23:[9y] 23:r 23:[wy] 23:r 23:[9yaG] 23:r
+23:[Qy] 23:[rId] 23:[9y] 23:r 23:[Qy] 23:r 23:[9ypG] 23:e 23:[Qy] 23:[eId] 23:[9y] 23:e 23:[Qy] 23:e 23:[0ua] 11:f 11:[rk] 11:a 11:[wuf] 11:k 11:[ra] 11:f 11:[0uk] 11:a 11:[rf] 11:k 11:[wua] 11:f 11:[rs] 11:f 11:[9ya] 11:d
+11:[rk] 11:a 11:[wyd] 11:k 11:[ra] 11:d 11:[9yk] 11:a 11:[rd] 11:k 11:[wya] 11:d 11:[rp] 11:d 11:[9yI] 11:a 11:[rG] 11:I 11:[Qya] 11:G 11:[rI] 11:a 11:[9yG] 11:I 11:[ra] 11:G 11:[QyI] 11:a 11:[ro] 11:a 11:[9yp] 11:d
+11:[ej] 11:p 11:[Qyd] 11:j 11:[ep] 11:d 11:[9yj] 11:p 11:[ed] 11:j 11:[Qyp] 11:d 11:[eo] 11:d 11:[0ua] 11:f 11:[rk] 11:a 11:[wuf] 11:k 11:[ra] 11:f 11:[0uk] 11:a 11:[rf] 11:k 11:[wua] 11:f 11:[rs] 11:f 11:[9ya] 11:d
+11:[rk] 11:a 11:[wyd] 11:k 11:[ra] 11:d 11:[9yk] 11:a 11:[rd] 11:k 11:[wya] 11:d 11:[rp] 11:d 11:[9yI] 11:a 11:[rG] 11:I 11:[Qya] 11:G 11:[rI] 11:a 11:[9yG] 11:I 11:[ra] 11:G 11:[QyI] 11:a 11:[ro] 11:a 11:[9yp] 11:d
+11:[ej] 11:p 11:[Qyd] 11:j 11:[ep] 11:d 11:[9yj] 11:p 11:[ed] 11:h 11:[QyG] 11:f 11:[eG] 11:h 11:[0uf] 11:a 11:[rk] 11:f 11:[wuh] 11:x 11:[rf] 11:h 11:[0ux] 11:f 11:[rh] 11:x 11:[wuf] 11:h 11:[rG] 11:h 11:[9yh] 11:k
+11:[rv] 11:h 11:[wyk] 11:v 11:[rh] 11:k 11:[9yv] 11:h 11:[rk] 11:v 11:[wyh] 11:k 11:[rG] 11:k 11:[9yd] 11:G 11:[rz] 11:d 11:[QyG] 11:z 11:[rd] 11:G 11:[9yz] 11:d 11:[rG] 11:z 11:[Qyd] 11:G 11:[rf] 11:G 11:[9yG] 11:j
+11:[eC] 11:G 11:[Qyj] 11:C 11:[eG] 11:j 11:[9yC] 11:G 11:[ez] 11:C 11:[Qyj] 11:z 11:[ev] 11:C 11:[0u] 23:[rv] 11:C 11:[wuv] 23:[rn] 11:m 11:[0un] 23:r 23:[wu] 23:r 23:[9y] 23:[rC] 11:v 11:[wyC] 23:[rv] 11:b 11:[9yv] 23:r
+23:[wy] 23:r 23:[9y] 23:[rC] 11:x 11:[QyC] 23:[rn] 11:m 11:[9yn] 23:r 23:[Qy] 23:r 23:[9y] 23:[eC] 11:x 11:[QyC] 23:e 23:[9y] 23:e 23:[Qy] 23:e 23:[0uhx] 23:r 23:[wu] 23:[rak] 23:[0u] 23:r 23:[wu] 23:r 23:[9ydz] 23:r 23:[wy]
+23:[rak] 23:[9y] 23:r 23:[wy] 23:r 23:[9yGC] 23:r 23:[Qy] 23:[rak] 23:[9y] 23:r 23:[Qy] 23:r 23:[9yGC] 23:e 23:[Qy] 23:[epj] 23:[9y] 23:e 23:[Qy] 23:e 23:[0ukv] 23:r 23:[wu] 23:[rhx] 23:[0u] 23:r 23:[wu] 23:r 23:[9ykv] 23:r 23:[wy]
+23:[rhz] 23:[9y] 23:r 23:[wy] 23:r 23:[9ykC] 23:r 23:[Qy] 23:[rGz] 23:[9y] 23:r 23:[Qy] 23:r 23:[9yjC] 23:e 23:[Qy] 23:[eGz] 23:[9y] 23:e 23:[Qy] 23:e 23:[0uk] 11:x 11:[rn] 11:k 11:[wux] 11:n 11:[rk] 11:x 11:[0un] 11:k 11:[rx]
+11:n 11:[wuk] 11:x 11:[rl] 11:x 11:[9yk] 11:z 11:[rn] 11:k 11:[wyz] 11:n 11:[rk] 11:z 11:[9yn] 11:k 11:[rz] 11:n 11:[wyk] 11:z 11:[rj] 11:z 11:[9yG] 11:k 11:[rC] 11:G 11:[Qyk] 11:C 11:[rG] 11:k 11:[9yC] 11:G 11:[rk]
+11:C 11:[QyG] 11:k 11:[rh] 11:k 11:[9yj] 11:z 11:[eb] 11:j 11:[Qyz] 11:b 11:[ej] 11:z 11:[9yb] 11:j 11:[ez] 11:b 11:[Qyj] 11:z 11:[eh] 11:z 11:[0uk] 11:x 11:[rn] 11:k 11:[wux] 11:n 11:[rk] 11:x 11:[0un] 11:k 11:[rx]
+11:n 11:[wuk] 11:x 11:[rl] 11:x 11:[9yk] 11:z 11:[rn] 11:k 11:[wyz] 11:n 11:[rk] 11:z 11:[9yn] 11:k 11:[rz] 11:n 11:[wyk] 11:z 11:[rj] 11:z 11:[9yG] 11:k 11:[rC] 11:G 11:[Qyk] 11:C 11:[rG] 11:k 11:[9yC] 11:G 11:[rk]
+11:C 11:[QyG] 11:k 11:[rh] 11:k 11:[9yj] 11:z 11:[eb] 11:j 11:[Qyz] 11:b 11:[ej] 11:z 11:[9yb] 11:j 11:[ez] 11:b 11:[Qyv] 11:C 11:[ex] 11:C 11:[0uv] 11:x 11:[rk] 10:x 10:[wuk] 11:h 10:[rk] 10:h 10:[0uf] 10:h 10:[rf]
+10:a 9:[wuf] 10:a 10:[ro] 10:f 9:[9yd] 10:a 9:[ro] 10:d 10:[wya] 9:o 9:[ra] 10:o 9:[9y] 10:o 9:[ry] 9:r 10:[9yo] 9:y 9:[Qr] 9:y 10:[27r] 9:Q 9:[9r] 9:y 9:[Qr] 9:y 10:[9I] 9:y 9:[rI] 9:a 9:[QI]
+9:a 9:[yd] 9:a 9:[rd] 8:G 9:[29j] 9:G 9:[wd] 9:G 9:[Qyd] 9:p 8:[wd] 9:G 9:[9j] 9:z 9:[eC] 8:z 9:[Qb] 9:C 8:[yz] 9:C 9:[30v] 8:x 9:[rk] 9:h 8:[wrf] 9:a 9:[uo] 8:a 9:[wrf] 8:h 9:[wk]
+8:x 9:[0v] 8:x 9:[7k] 8:h 9:[29v] 8:z 9:[rk] 8:h 8:[wrd] 9:a 8:[yo] 9:a 8:[rd] 8:h 9:[wk] 8:z 8:[9v] 9:z 8:[yk] 8:h 9:[QC] 8:z 8:[yk] 9:G 8:[rd] 8:a 8:[Qd] 8:G 9:[yk] 8:z 8:[rC]
+8:n 8:[Qn] 9:C 8:[9z] 8:n 8:[9eb] 8:C 8:[Qz] 8:b 9:[yC] 8:z 8:[ej] 8:G 8:[Qd] 8:p 8:[$9I] 8:y 8:[6e] 8:Q 8:[29] 8:6 8:3 17:[7w] 8:Q 8:[0w] 17:[7r] 8:t 8:[3r] 17:7 17:w 17:7 17:2
+17:[7Q] 8:w 8:[9Q] 17:[7w] 8:e 8:[2w] 17:7 17:9 8:w 8:[7r] 8:o 8:2 17:[7Q] 8:0 8:[9Q] 17:[7r] 8:t 8:[2r] 17:7 8:r 8:[9I] 8:y 8:[7r] 8:y 8:2 17:[6Q] 8:0 8:[9Q] 17:6 17:2 8:e 8:[9y]
+8:I 8:[Qp] 8:d 8:[eG] 8:j 8:[30] 17:[ro] 8:I 8:[wuo] 17:[ra] 8:s 8:[0ua] 17:r 17:[wu] 17:r 17:[29] 17:[rI] 8:o 8:[wyI] 17:[ro] 8:p 8:[9yo] 17:r 17:[wy] 17:r 17:[29] 17:[rI] 8:u 8:[QyI] 17:[ra] 8:s 8:[9ya]
+17:r 17:[Qy] 17:r 17:[29] 17:[eI] 8:u 8:[QyI] 17:e 17:[9y] 17:e 8:p 8:[Qyd] 8:p 8:[ed] 8:G 8:[30h] 8:f 8:[ra] 8:h 8:[wuf] 8:a 8:[rf] 8:a 8:[30f] 8:a 8:[ro] 8:a 8:[wuo] 8:a 8:[rf] 8:a 8:[29h]
+8:d 8:[ra] 8:d 8:[wya] 8:o 8:[ra] 8:o 8:[29d] 8:a 8:[ro] 8:a 8:[wyo] 8:a 8:[rd] 17:[29G] 8:d 8:[ra] 8:G 8:[Qyd] 8:a 8:[rd] 8:a 8:[29d] 8:a 8:[rI] 8:a 8:[QyI] 8:a 8:[rI] 17:[29G] 8:d 8:[ep]
+8:G 8:[Qyd] 8:p 8:[ed] 8:p 8:[29d] 8:p 8:[eI] 8:y 8:[QyI] 8:p 8:[ed] 8:G 8:[0u] 17:[rk] 8:h 9:[wuf] 8:k 8:[rh] 9:f 8:[0uh] 9:f 8:[ra] 8:f 9:[wua] 8:o 9:[ra] 18:[9y] 18:[rk] 8:h 9:[wyd] 8:h
+9:[rd] 9:a 8:[9yd] 9:a 8:[ro] 9:d 9:[wya] 8:o 9:r 18:[9y] 18:[rz] 9:k 9:[QyG] 8:k 9:[rG] 9:d 8:[9yG] 9:d 9:[ra] 8:G 9:[Qyd] 9:a 9:r 18:[9y] 19:[eC] 8:z 9:[Qyj] 9:z 9:[ej] 8:G 9:[9yd] 9:p
+9:[eI] 8:y 9:[QyI] 9:p 9:[ep] 9:d 8:[0u] 19:[rk] 9:x 9:[wun] 9:k 8:[rx] 9:n 9:[0uk] 9:x 9:[rn] 8:k 9:[wux] 9:l 9:[rx] 9:n 9:[9yk] 8:z 9:[rn] 9:k 9:[wyz] 9:n 8:[rk] 9:z 9:[9yn] 9:k 9:[rz]
+9:n 8:[wyk] 9:z 9:[rj] 9:z 9:[9yG] 8:k 9:[rC] 9:G 9:[Qyk] 9:C 9:[rG] 8:k 9:[9yC] 9:G 9:[rk] 9:C 8:[QyG] 9:k 9:[rh] 9:k 9:[9yj] 9:z 8:[eb] 9:j 9:[Qyz] 9:b 9:[ej] 8:z 9:[9yb] 9:j 9:[ez]
+9:b 9:[Qyj] 8:z 9:[eh] 9:z 9:[0uk] 9:x 8:[rn] 9:k 9:[wux] 9:n 9:[rk] 9:x 8:[0un] 9:k 9:[rx] 9:n 9:[wuk] 8:x 9:[rl] 9:x 9:[9yk] 9:z 9:[rn] 8:k 9:[wyz] 9:n 9:[rk] 9:z 8:[9yn] 9:k 9:[rz]
+9:n 9:[wyk] 9:z 8:[rj] 9:z 9:[9yG] 9:k 9:[rC] 8:G 9:[Qyk] 9:C 9:[rG] 9:k 9:[9yC] 8:G 9:[rk] 9:C 9:[QyG] 9:k 8:[rh] 9:k 9:[9yj] 9:z 9:[eb] 9:j 8:[Qyz] 9:b 9:[ej] 9:z 9:[9yb] 8:j 9:[ez]
+9:b 9:[Qyh] 9:z 9:[ej] 8:z 9:[0uk] 9:x 9:[an] 9:k 8:[ofx] 9:n 9:[ak] 9:x 9:[ufn] 9:k 8:[ax] 9:n 9:[ofk] 9:x 9:[al] 8:x 9:[ydk] 9:z 9:[an] 9:k 9:[odz] 8:n 9:[ak] 9:z 9:[ydn] 9:k 8:[az]
+9:n 9:[odk] 9:z 9:[aj] 9:z 8:[ydG] 9:k 9:[aC] 9:G 9:[Idk] 8:C 9:[aG] 9:k 9:[ydC] 9:G 9:[ak] 8:C 9:[IdG] 9:k 9:[ah] 9:k 8:[ydj] 9:z 9:[pb] 9:j 9:[Idz] 9:b 8:[pj] 9:z 9:[ydb] 9:j 9:[pz]
+8:b 9:[Idk] 9:z 9:[pl] 9:z 9:[ufn] 8:x 9:[an] 9:n 9:[ofx] 9:n 8:[an] 9:x 9:[ufn] 9:n 9:[ax] 9:n 8:[ofn] 9:x 9:[am] 9:x 9:[ydn] 8:z 9:[an] 9:n 9:[odz] 9:n 9:[an] 8:z 9:[ydn] 9:n 9:[az]
+9:n 8:[odn] 9:z 9:[ab] 9:z 9:[ydC] 9:n 8:[aC] 9:C 9:[Idn] 9:C 9:[aC] 8:n 9:[ydC] 9:C 9:[an] 9:C 9:[IdC] 8:n 9:[av] 9:n 9:[ydb] 9:z 8:[pb] 9:b 9:[Idz] 9:b 9:[pb] 9:z 8:[ydb] 9:b 9:[pz]
+9:v 9:[IdC] 8:x 9:[pC] 9:v 9:[ufx] 9:n 9:[ax] 8:v 9:[ofx] 9:n 9:[av] 9:x 8:[fn] 9:v 9:[ax] 9:n 9:[ov] 9:x 8:[un] 9:v 9:[ydz] 9:n 9:[ov] 8:z 9:[an] 9:v 9:[dz] 9:n 9:[av] 8:z 9:[on]
+9:v 9:[yz] 9:n 8:[ov] 9:z 9:[yn] 9:C 9:[Iz] 9:n 8:[aC] 9:z 9:[dn] 9:C 9:[Gz] 8:n 9:[dC] 9:z 9:[an] 9:C 9:[Iz] 8:n 9:[yC] 9:z 9:[Ib] 9:C 8:[pz] 9:b 9:[dC] 9:z 9:[Gb] 9:C 8:[dz]
+9:b 9:[pC] 9:z 9:[Ib] 8:C 9:[uz] 9:n 9:[ox] 9:n 9:[av] 8:n 9:[fv] 9:x 9:[kn] 9:v 8:[hx] 9:v 9:[fx] 9:k 9:[ax] 9:k 8:[yz] 9:k 9:[ah] 9:z 9:[ok] 8:h 9:[dk] 9:z 9:[az] 9:v 9:[on]
+8:v 9:[yz] 9:v 9:[az] 9:k 8:[IC] 9:z 9:[dk] 9:G 9:[az] 9:k 8:[IG] 9:d 9:[yk] 9:G 9:[rd] 8:a 9:[QG] 9:d 9:[9a] 9:I 9:[ep] 8:I 9:[Qy] 9:u 9:[9I] 9:o 8:[ep] 9:a 9:[Qs] 9:d 9:[9f]
+5:G 6:h 5:[ej] 6:k 5:l 6:[9z] 6:x 5:C 6:[0v] 23:r 23:o 23:[rk] 23:0 23:r 23:o 23:r 23:[9v] 23:r 23:o 23:[rz] 23:9 23:r 23:o 23:r 23:[9C] 23:r 23:I 23:[rk] 23:9 23:r 23:I 23:r
+23:[9C] 23:e 23:I 23:[ej] 23:9 23:e 23:I 23:[ep] 23:[0o] 23:r 23:u 23:[af] 23:o 23:a 23:o 23:u 23:[yh] 23:o 23:a 23:[rd] 23:o 23:a 23:o 23:y 23:[rG] 23:y 23:I 23:[ad] 23:I 23:y 23:I 23:a
+23:[yG] 23:I 23:p 23:d 23:I 23:p 23:d 23:G 23:u 23:[ah] 11:G 11:[fh] 23:k 11:l 11:k 95:y 23:[aG] 11:h 11:[oG] 23:[dh] 11:j 11:h 95:y 23:[IG] 11:f 11:[aG] 23:[dk] 11:l 11:k 95:y 23:[IG] 11:f
+11:[pG] 23:d 23:G 23:j 23:z 23:C 23:f 23:[kv] 11:C 11:[xv] 23:n 11:m 11:n 95:d 23:[hC] 11:v 11:[kC] 23:[zv] 11:b 11:v 95:d 23:[GC] 11:x 11:[kC] 23:n 11:m 11:n 95:d 23:[GC] 13:x 14:[jC] 121:z
+69:l
+]=]
+
+local sheet = {}
+for encoded in DATA:gmatch("%S+") do
+    local gap, token = encoded:match("^(%d+):(.+)$")
+    gap = tonumber(gap)
+    if gap and token then
+        for _ = 1, gap do
+            sheet[#sheet + 1] = "-"
+        end
+        sheet[#sheet + 1] = token
+    end
+end
+
+return {
+    Id="comptine-dun-autre-ete",
+    Name="Comptine d'un autre été",
+    Artist="Yann Tiersen",
+    BPM=90,
+    StepsPerBeat=48,
+    Complete=true,
+    Source="User-supplied MIDI",
+    SourceLicense="User-provided source file; arrangement converted for Velora.",
+    Categories={"Famous","French","Piano","Soundtrack","Emotional","Yann Tiersen","Complete"},
+    Notes=table.concat(sheet, " "),
+}
