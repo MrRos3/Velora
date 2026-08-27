1612181653b26126027a5929931d93eca32bfe14
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-27 16:48:45 +0400

    Add Rain by Tony Ann MIDI arrangement

---
+++ songs/RainTonyAnn.lua
@@ -0,0 +1,82 @@
+-- Velora 0.10.20 Nova — complete arrangement converted from a MIDI supplied by the repository owner.
+-- Supplied file: rain-tony-ann.mid
+-- Supplied file SHA-256: 53461A234D62B659AC5EF94119E7979996F6A973ADFDDD8A925A8ADA9B4FF579
+-- MIDI: 480 PPQ, 5 source tempo events, source time signature: 4/4.
+-- Timing normalized to real seconds at 77 BPM / 48 Velora steps per beat, preserving the source ritardando.
+-- Both piano tracks retained; simultaneous duplicate pitches are merged.
+-- 81 out-of-range note-on events were folded by octave into Velora's C2-C7 keyboard range.
+
+local DATA = [=[
+0:d 47:z 47:d 47:z 47:d 47:z 47:d 47:z 47:[od] 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:d 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:[id] 11:j 11:h 11:g
+11:z 11:j 11:h 11:g 11:d 11:j 11:h 11:g 11:z 11:j 11:h 11:g 11:[Yd] 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:d 11:J 11:j 11:h 11:z 11:J 11:j 11:h
+11:[yd] 11:j 11:h 11:G 11:z 11:j 11:h 11:G 11:d 11:j 11:h 11:G 11:z 11:j 11:h 11:G 11:[Dh] 11:[Pj] 11:[oJ] 11:[Yl] 11:[Dz] 11:[Pl] 11:[oJ] 11:[Yj] 11:[Dh] 11:[Pj] 11:[oJ] 11:[Yl]
+11:[Dz] 11:[Pl] 11:[oJ] 11:[Yj] 11:[dJ] 11:[PB] 11:[oJ] 11:[yB] 11:[dJ] 11:[PB] 11:[oJ] 11:[yB] 11:[dJ] 11:[PB] 11:[oJ] 11:[yB] 11:[dJ] 11:[PB] 11:[oJ] 11:[yB] 11:[sg] 11:[ph] 11:[ij] 11:[tJ] 11:[sl] 11:[pJ] 11:[ij] 11:[th]
+11:[sg] 11:[ph] 11:[ij] 11:[tJ] 11:[sl] 11:[pJ] 11:[ij] 11:[th] 11:[dg] 11:[pc] 11:[ig] 11:[yc] 11:[dg] 11:[pc] 11:[ig] 11:[yc] 11:[dg] 11:[pc] 11:[ig] 11:[yc] 11:[dg] 11:[pc] 11:[ig] 11:[yc] 11:[(h] 11:[Ej] 11:[YJ] 11:[ol]
+11:[Pz] 11:[ol] 11:[YJ] 11:[Ej] 11:[(h] 11:[Ej] 11:[YJ] 11:[ol] 11:[Pz] 11:[ol] 11:[YJ] 11:[Ej] 11:[wJ] 11:[yB] 11:[oJ] 11:[PB] 11:[dJ] 11:[PB] 11:[oJ] 11:[yB] 11:[wJ] 11:[yB] 11:[oJ] 11:[PB] 11:[dJ] 11:[PB] 11:[oJ] 11:[yB]
+11:[qg] 11:[th] 11:[ij] 11:[pJ] 11:[sl] 11:[pJ] 11:[ij] 11:[th] 11:[qg] 11:[th] 11:[ij] 11:[pJ] 11:[sl] 11:[pJ] 11:[ij] 11:[th] 11:[9g] 7:j 7:z 7:[ec] 7:b 7:z 7:[yc] 7:z 7:b 7:[ic] 7:z 7:j
+7:[pg] 7:d 7:p 7:i 7:y 7:e 7:q 7:e 7:y 7:i 7:y 7:e 7:[5o] 7:y 7:E 7:o 7:y 7:E 7:[oh] 7:y 7:E 7:o 7:y 7:E 7:[5o] 7:y 7:E 7:o
+7:y 7:E 7:[oh] 7:y 7:E 7:o 7:y 7:E 7:[4p] 7:i 7:y 7:p 7:i 7:y 7:[pj] 7:i 7:y 7:p 7:i 7:y 7:[4p] 7:i 7:y 7:p 7:i 7:y 7:[pj] 7:i
+7:y 7:p 7:i 7:y 7:[@P] 7:o 7:y 7:P 7:o 7:y 7:[PJ] 7:o 7:y 7:P 7:o 7:y 7:[@P] 7:o 7:y 7:P 7:o 7:y 7:[sJ] 7:o 7:y 7:s 7:o 7:y
+7:[2yIpd] 47:[dz] 47:z 23:[lm] 23:[JB] 23:[jb] 23:[1hv] 11:5 11:8 11:( 11:w 11:t 11:Y 11:o 11:s 11:o 11:Y 11:t 11:w 11:( 11:8 11:5 11:[^dz] 11:q 11:E 11:y 11:i 11:P
+11:d 11:g 11:J 11:g 11:d 11:P 11:i 11:y 11:E 11:q 11:[5JB] 11:9 11:w 11:E 11:y 11:o 11:P 11:d 11:h 11:d 11:P 11:o 11:[ydz] 11:E 11:w 11:9 11:[4sl] 11:8
+11:q 11:e 11:[tjb] 11:i 11:p 11:s 11:[gJB] 11:s 11:p 11:i 11:[tjb] 11:e 11:[qsl] 11:[8dz] 11:[1DZ] 11:5 11:8 11:( 11:w 11:t 11:Y 11:o 11:s 11:o 11:Y 11:t 11:[whv] 11:(
+11:8 11:5 11:[5JB] 11:9 11:w 11:E 11:y 11:o 11:P 11:d 11:h 11:d 11:P 11:o 11:[ylm] 11:E 11:w 11:9 11:[^z] 11:q 11:E 11:y 11:i 11:P 11:d 11:g 11:J 11:g
+11:d 11:P 11:[ilm] 11:y 11:E 11:q 11:[2z] 7:6 7:9 7:Q 7:e 7:y 7:[Ilm] 7:y 7:e 7:Q 7:9 7:6 7:[2JB] 7:6 7:9 7:Q 7:e 7:y 7:[Ijb] 7:y 7:e 7:Q
+7:9 7:6 7:h 47:v 47:h 47:v 47:h 47:v 47:h 47:v 47:[od] 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:d 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:[id] 11:j
+11:h 11:g 11:z 11:j 11:h 11:g 11:d 11:j 11:h 11:g 11:z 11:j 11:h 11:g 11:[Yd] 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:d 11:J 11:j 11:h 11:z 11:J
+11:j 11:h 11:[yd] 11:j 11:h 11:G 11:z 11:j 11:h 11:G 11:h 11:G 11:f 11:d 11:s 11:P 11:p 11:P 11:[8wtYo] 143:t 47:[^qEy] 191:[59wEP] 143:y 47:[48qet] 47:p 47:P 47:p 23:s
+11:d 11:[8D] 23:w 23:Y 23:w 23:8 23:w 23:[Yh] 23:w 23:[5PJ] 23:9 23:E 23:9 23:5 23:9 23:[Esl] 23:9 23:[^dgJz] 23:q 23:y 23:q 23:^ 23:q 23:[ysl] 23:q 23:[2dGjz] 11:6 11:9
+11:Q 11:[esl] 11:Q 11:9 11:6 11:[2PJ] 11:6 11:9 11:Q 11:[epj] 11:Q 11:9 11:6 11:[Dh] 11:j 11:J 11:l 11:[Pz] 11:l 11:J 11:j 11:[oh] 11:j 11:J 11:l 11:[Yz] 11:l 11:J
+11:j 11:[yJ] 11:[oB] 11:[PJ] 11:[dB] 11:[yJ] 11:[oB] 11:[PJ] 11:[dB] 11:[yJ] 11:[oB] 11:[PJ] 11:[dB] 11:[yJ] 11:[oB] 11:[PJ] 11:[dB] 11:[sg] 11:h 11:j 11:J 11:[pl] 11:J 11:j 11:h 11:[ig] 11:h 11:j
+11:J 11:[tl] 11:J 11:j 11:h 11:[yg] 11:[ic] 11:[pg] 11:[dc] 11:[yg] 11:[ic] 11:[pg] 11:[dc] 11:[yg] 11:[ic] 11:[pg] 11:[dc] 11:[yg] 11:[ic] 11:[pg] 11:[dc] 11:[Yoh] 11:[Epj] 11:[wPJ] 11:[Esl] 11:[Ydz] 11:[Esl] 11:[wPJ]
+11:[Epj] 11:[Yoh] 11:[Epj] 11:[wPJ] 11:[Esl] 11:[Ydz] 11:[Esl] 11:[wPJ] 11:[Epj] 11:[yP] 11:[EJ] 11:[wB] 11:[EJ] 11:[yP] 11:[EJ] 11:[wB] 11:[EJ] 11:[yP] 11:[EJ] 11:[wB] 11:[EJ] 11:[yP] 11:[EJ] 11:[wB] 11:[EJ] 11:[tig] 11:[eoh] 11:[qpj]
+11:[ePJ] 11:[tsl] 11:[ePJ] 11:[qpj] 11:[eoh] 11:[tig] 11:[eoh] 11:[qpj] 11:[ePJ] 11:[tsl] 11:[ePJ] 11:[qpj] 11:[eoh] 11:[yi] 11:[eg] 11:[9c] 11:[eg] 11:[yi] 11:[eg] 11:[9c] 11:[eg] 11:[yi] 11:[eg] 11:[9c] 11:[eg] 11:[yp] 11:[ej] 11:[9b]
+11:[ej] 11:[(h] 7:d 7:P 7:[(h] 7:d 7:P 7:[@h] 7:d 7:P 7:[(h] 7:d 7:P 7:[(h] 7:d 7:P 7:[(h] 7:d 7:P 7:[@h] 7:d 7:P 7:[(h] 7:d 7:P 7:[qj] 7:g 7:d
+7:[qj] 7:g 7:d 7:[4j] 7:g 7:d 7:[qj] 7:g 7:d 7:[qj] 7:g 7:d 7:[qj] 7:g 7:d 7:[4j] 7:g 7:d 7:[qj] 7:g 7:d 7:s 7:[wJ] 7:h 7:d 7:[wJ] 7:h 7:d 7:[5J]
+7:h 7:d 7:[wJ] 7:h 7:d 7:[wJ] 7:h 7:d 7:[wJ] 7:h 7:d 7:[5wJ] 7:h 7:d 7:[6el] 7:j 7:g 7:[^Ez] 7:J 7:g 7:[ic] 7:J 7:g 7:[yz] 7:J 7:g 7:[tl] 7:j
+7:g 7:[9qEJ] 7:g 7:d 7:[tl] 7:g 7:d 7:[EJ] 7:g 7:d 7:[qg] 7:d 7:p 7:[(h] 7:d 7:P 7:[(o] 7:y 7:E 7:[@w] 7:E 7:y 7:[(o] 7:P 7:d 7:[@h] 7:d 7:P
+7:[@o] 7:y 7:E 7:[@w] 7:E 7:y 7:[@o] 7:P 7:d 7:[4j] 7:g 7:d 7:[4p] 7:i 7:y 7:[4e] 7:y 7:i 7:[4p] 7:d 7:g 7:[4j] 7:g 7:d 7:[4p] 7:i 7:y 7:[4e]
+7:y 7:i 7:[4p] 7:d 7:h 7:[5J] 7:h 7:d 7:[5P] 7:o 7:y 7:[5E] 7:y 7:o 7:[5P] 7:d 7:h 7:[5J] 7:h 7:d 7:[5P] 7:o 7:y 7:[5E] 7:y 7:o 7:[5P] 7:d
+7:h 7:[2pdgj] 47:b 47:[qi] 11:[wo] 11:[ep] 11:[EP] 11:[ts] 11:[yd] 11:[YD] 11:[ig] 11:[1osDh] 11:5 11:8 11:( 11:w 11:t 11:Y 11:o 11:s 11:o 11:Y 11:t 11:[wlm] 11:( 11:8 11:5 11:[^zcB]
+11:q 11:E 11:y 11:i 11:P 11:d 11:g 11:J 11:g 11:d 11:P 11:i 11:y 11:[Ehv] 11:[qjb] 11:[5JzvB] 11:9 11:w 11:E 11:y 11:o 11:P 11:d 11:[hB] 11:d 11:P 11:o 11:[yz]
+11:E 11:w 11:9 11:[4lcbm] 11:8 11:q 11:e 11:[tjlcb] 11:i 11:p 11:s 11:[gJB] 11:s 11:p 11:i 11:[tjb] 11:e 11:[qlm] 11:[8z] 11:[1Zvm] 11:5 11:8 11:( 11:w 11:t 11:Y 11:o 11:s
+11:o 11:Y 11:t 11:[whv] 11:( 11:8 11:5 11:[5PdhJ] 11:9 11:w 11:E 11:y 11:o 11:P 11:d 11:h 11:d 11:P 11:o 11:[ysl] 11:E 11:w 11:9 11:[^dgJz] 11:4 11:^ 11:9 11:q
+11:E 11:y 11:i 11:P 11:i 11:y 11:E 11:[qsl] 11:9 11:^ 11:4 11:[2dGjz] 7:6 7:9 7:Q 7:e 7:y 7:[Isl] 7:y 7:e 7:Q 7:9 7:6 7:[2PJ] 7:6 7:9 7:Q 7:e
+7:y 7:[Ipj] 7:y 7:e 7:Q 7:9 7:6 7:[@oh] 7:^ 7:( 7:w 7:E 7:Y 7:[ohv] 7:Y 7:E 7:w 7:( 7:^ 7:[@oh] 7:^ 7:( 7:w 7:E 7:Y 7:[ohv] 7:Y 7:E
+7:w 7:( 7:^ 7:[4pj] 7:8 7:q 7:e 7:t 7:i 7:[pjb] 7:i 7:t 7:e 7:q 7:8 7:[4pj] 7:8 7:q 7:e 7:t 7:i 7:[pjb] 7:i 7:t 7:[eig] 7:q 7:8 7:[5PJ]
+7:9 7:w 7:E 7:y 7:o 7:[PJB] 7:o 7:y 7:E 7:w 7:9 7:[5PJ] 7:9 7:w 7:E 7:y 7:o 7:[PJB] 7:o 7:y 7:[Elm] 7:w 7:9 7:[^z] 7:q 7:E 7:[yc] 7:i
+7:P 7:[dz] 7:P 7:i 7:[ylm] 7:E 7:q 7:[2JB] 7:6 7:9 7:[qlm] 7:e 7:y 7:[iJB] 7:y 7:e 7:[qgc] 7:9 7:6 7:[@h] 7:[^J] 7:[(Z] 7:[wv] 7:[EB] 7:[YZ] 7:[ov] 7:[YZ] 7:[EB]
+7:[wv] 7:[(Z] 7:[^J] 7:[@h] 7:[^J] 7:[(Z] 7:[wv] 7:[EB] 7:[YZ] 7:[ov] 7:[YZ] 7:[EB] 7:[wv] 7:[(Z] 7:[^J] 7:[4j] 7:[8l] 7:[qc] 7:[eb] 7:[tm] 7:[ic] 7:[pb] 7:[ic] 7:[tm] 7:[eb] 7:[qc] 7:[8l] 7:[2j]
+7:[6z] 7:[9c] 7:[qb] 7:[ez] 7:[yc] 7:[ib] 7:[yc] 7:[ez] 7:[qb] 7:[9c] 7:[6z] 7:[5J] 7:[9z] 7:[wv] 7:[EB] 7:[yz] 7:[ov] 7:[PB] 7:[ov] 7:[yz] 7:[EB] 7:[wv] 7:[9z] 7:[5J] 7:z 7:v 7:z 7:v
+7:B 7:[5v] 7:B 7:z 7:B 7:z 7:v 7:[2zCb] 47:C 47:z 47:[jb] 47:[GC] 47:[dz] 47:[pj] 47:[IG] 47:[yd] 47:[dz] 47:z 47:[dz] 47:[yd] 47:z 47:z 47:z 47:d 47:z 47:d 47:z 47:d
+47:z 47:d 47:z 47:[od] 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:d 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:[id] 11:j 11:h 11:g 11:z 11:j 11:h 11:g 11:d
+11:j 11:h 11:g 11:z 11:j 11:h 11:g 11:[Yd] 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:d 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:[yd] 11:j 11:h 11:G 11:z
+11:j 11:h 11:G 11:d 11:j 11:h 11:G 11:z 11:j 11:h 11:G 11:[od] 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:d 11:J 11:[jz] 11:h 11:z 11:J 11:[jz] 11:h 11:[id]
+11:j 11:h 11:g 11:z 11:j 11:h 11:g 11:d 11:j 11:[hz] 11:g 11:z 11:j 11:[hz] 11:g 11:[Yd] 11:J 11:j 11:h 11:z 11:J 11:j 11:h 11:d 11:J 11:[jz] 11:h 11:z
+11:J 11:[jz] 11:h 11:[yId] 11:j 11:h 11:G 11:z 11:j 11:h 11:G 11:d 12:j 12:h 12:G 11:z 13:j 13:h 14:G
+]=]
+
+local sheet = {}
+for encoded in DATA:gmatch("%S+") do
+    local gap, token = encoded:match("^(%d+):(.+)$")
+    gap = tonumber(gap)
+    if gap and token then
+        for _ = 1, gap do
+            sheet[#sheet + 1] = "-"
+        end
+        sheet[#sheet + 1] = token
+    end
+end
+
+return {
+    Id="rain-tony-ann",
+    Name="Rain",
+    Artist="Tony Ann",
+    BPM=77,
+    StepsPerBeat=48,
+    Complete=true,
+    Source="User-supplied MIDI",
+    SourceLicense="User-provided source file; arrangement converted for Velora.",
+    Categories={"Famous","Piano","Instrumental","Emotional","Modern Classical","Tony Ann","Complete"},
+    Notes=table.concat(sheet, " "),
+}
