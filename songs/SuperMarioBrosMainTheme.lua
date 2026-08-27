56bd89a64cd5374283438f51cae9e5622490328a
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-27 17:10:00 +0400

    Add Super Mario Bros Main Theme MIDI arrangement

---
+++ songs/SuperMarioBrosMainTheme.lua
@@ -0,0 +1,50 @@
+-- Velora 0.10.20 Nova — complete arrangement converted from a MIDI supplied by the repository owner.
+-- Supplied file: super-mario-bros-main-theme.mid
+-- Supplied file SHA-256: C42B6DACAFFC3B5F0A27024E91252B50FFF15B5343F3650EBDCBF183523D1943
+-- MIDI: 480 PPQ, 1 source tempo event, source time signature(s): 4/4.
+-- Timing normalized to real seconds at 180 BPM / 48 Velora steps per beat.
+-- Both non-drum tracks retained; simultaneous duplicate pitches are merged.
+-- 3 out-of-range note-on events were folded by octave into Velora's C2-C7 keyboard range.
+
+local DATA = [=[
+0:[9If] 23:[9If] 47:[9If] 47:[9Is] 23:[9If] 47:[woah] 95:[5o] 95:[wus] 71:[0to] 71:[8wu] 71:[qtp] 47:[wya] 47:[QTP] 23:[qtp] 47:[0to] 31:[tof] 31:[uah] 31:[isj] 47:[ypg] 23:[uah] 47:[tpf] 47:[eus] 23:[rid] 23:[wya] 71:[wus] 71:[0to] 71:[8wu] 71:[qtp] 47:[wya] 47:[QTP] 23:[qtp] 47:[0to]
+31:[tof] 31:[uah] 31:[isj] 47:[ypg] 23:[uah] 47:[tpf] 47:[eus] 23:[rid] 23:[wya] 71:8 47:[fh] 23:[wDG] 23:[dg] 23:[aD] 23:t 23:[sf] 23:q 23:[uO] 23:[ip] 23:[tos] 23:t 23:[tp] 23:[qus] 23:[id] 23:8 47:[fh] 23:[wDG] 23:[dg] 23:[aD] 23:w 23:[tsf] 47:[ghl]
+47:[ghl] 23:[ghl] 47:w 47:8 47:[fh] 23:[wDG] 23:[dg] 23:[aD] 23:t 23:[sf] 23:q 23:[uO] 23:[ip] 23:[tos] 23:t 23:[tp] 23:[qus] 23:[id] 23:8 47:[WOD] 71:[Eid] 71:[tus] 71:w 23:w 47:8 47:8 47:[fh] 23:[wDG] 23:[dg] 23:[aD] 23:t 23:[sf]
+23:q 23:[uO] 23:[ip] 23:[tos] 23:t 23:[tp] 23:[qus] 23:[id] 23:8 47:[fh] 23:[wDG] 23:[dg] 23:[aD] 23:w 23:[tsf] 47:[ghl] 47:[ghl] 23:[ghl] 47:w 47:8 47:[fh] 23:[wDG] 23:[dg] 23:[aD] 23:t 23:[sf] 23:q 23:[uO] 23:[ip] 23:[tos] 23:t 23:[tp]
+23:[qus] 23:[id] 23:8 47:[WOD] 71:[Eid] 71:[tus] 71:w 23:w 47:8 47:[%Os] 23:[Os] 47:[(Os] 47:[Os] 23:[WPd] 47:[wof] 23:[us] 47:[8up] 23:[to] 47:5 47:[%Os] 23:[Os] 47:[(Os] 47:[Os] 23:[WPd] 23:[of] 23:w 71:8 71:5 47:[%Os] 23:[Os] 47:[(Os] 47:[Os]
+23:[WPd] 47:[wof] 23:[us] 47:[8up] 23:[to] 47:5 47:[9If] 23:[9If] 47:[9If] 47:[9Is] 23:[9If] 47:[woah] 95:[5o] 95:[wus] 71:[0to] 71:[8wu] 71:[qtp] 47:[wya] 47:[QTP] 23:[qtp] 47:[0to] 31:[tof] 31:[uah] 31:[isj] 47:[ypg] 23:[uah] 47:[tpf] 47:[eus] 23:[rid] 23:[wya] 71:[wus] 71:[0to]
+71:[8wu] 71:[qtp] 47:[wya] 47:[QTP] 23:[qtp] 47:[0to] 31:[tof] 31:[uah] 31:[isj] 47:[ypg] 23:[uah] 47:[tpf] 47:[eus] 23:[rid] 23:[wya] 71:[8sf] 23:[ps] 47:[Quo] 23:w 47:[tuO] 47:[qip] 23:[sg] 23:q 23:[sg] 23:[tip] 23:t 23:q 47:[9oa] 31:[gj] 31:[gj] 7:q 23:[wgj]
+31:[fh] 15:r 15:[dg] 31:[qsf] 23:[ps] 23:q 23:[ip] 23:[tuo] 23:t 23:q 47:[8sf] 23:[ps] 47:[Quo] 23:w 47:[tuO] 47:[qip] 23:[sg] 23:q 23:[sg] 23:[tip] 23:t 23:q 47:[woa] 23:[wdg] 47:[wdg] 23:[wdg] 31:[esf] 31:[rad] 31:[tos] 23:u 23:w 23:u
+23:[8t] 95:[8sf] 23:[ps] 47:[Quo] 23:w 47:[tuO] 47:[qip] 23:[sg] 23:q 23:[sg] 23:[tip] 23:t 23:q 47:[9oa] 31:[gj] 31:[gj] 7:q 23:[wgj] 31:[fh] 15:r 15:[dg] 31:[qsf] 23:[ps] 23:q 23:[ip] 23:[tuo] 23:t 23:q 47:[8sf] 23:[ps] 47:[Quo] 23:w
+47:[tuO] 47:[qip] 23:[sg] 23:q 23:[sg] 23:[tip] 23:t 23:q 47:[woa] 23:[wdg] 47:[wdg] 23:[wdg] 31:[esf] 31:[rad] 31:[tos] 23:u 23:w 23:u 23:[8t] 95:[%Os] 23:[Os] 47:[(Os] 47:[Os] 23:[WPd] 47:[wof] 23:[us] 47:[8up] 23:[to] 47:5 47:[%Os] 23:[Os] 47:[(Os]
+47:[Os] 23:[WPd] 23:[of] 23:w 47:x 23:[8v] 23:x 23:m 23:[5z] 23:v 23:[%Os] 23:[Os] 47:[(Os] 47:[Os] 23:[WPd] 47:[wof] 23:[us] 47:[8up] 23:[to] 47:5 47:[9If] 23:[9If] 47:[9If] 47:[9Is] 23:[9If] 47:[woah] 95:[5o] 95:[8sf] 23:[ps] 47:[Quo] 23:w 47:[tuO]
+47:[qip] 23:[sg] 23:q 23:[sg] 23:[tip] 23:t 23:q 47:[9oa] 31:[gj] 31:[gj] 7:q 23:[wgj] 31:[fh] 15:r 15:[dg] 31:[qsf] 23:[ps] 23:q 23:[ip] 23:[tuo] 23:t 23:q 47:[8sf] 23:[ps] 47:[Quo] 23:w 47:[tuO] 47:[qip] 23:[sg] 23:q 23:[sg] 23:[tip]
+23:t 23:q 47:[woa] 23:[wdg] 47:[wdg] 23:[wdg] 31:[esf] 31:[rad] 31:[tos] 23:u 23:w 23:u 23:[8t] 95:[8sf] 23:[ps] 47:[Quo] 23:w 47:[tuO] 47:[qip] 23:[sg] 23:q 23:[sg] 23:[tip] 23:t 23:q 47:[9oa] 31:[gj] 31:[gj] 7:q 23:[wgj] 31:[fh] 15:r
+15:[dg] 31:[qsf] 23:[ps] 23:q 23:[ip] 23:[tuo] 23:t 23:q 47:[8sf] 23:[ps] 47:[Quo] 23:w 47:[tuO] 47:[qip] 23:[sg] 23:q 23:[sg] 23:[tip] 23:t 23:q 47:[woa] 23:[wdg] 47:[wdg] 23:[wdg] 31:[esf] 31:[rad] 31:[tos] 23:u 23:w 23:u 23:[8t] 95:[wus]
+71:[0to] 71:[8wu] 47:[48ip] 31:a 31:p 31:[!%iO] 31:P 31:O 31:[15uo] 23:y 23:u
+]=]
+
+local sheet = {}
+for encoded in DATA:gmatch("%S+") do
+    local gap, token = encoded:match("^(%d+):(.+)$")
+    gap = tonumber(gap)
+    if gap and token then
+        for _ = 1, gap do
+            sheet[#sheet + 1] = "-"
+        end
+        sheet[#sheet + 1] = token
+    end
+end
+
+return {
+    Id="super-mario-bros-main-theme",
+    Name="Super Mario Bros. Main Theme",
+    Artist="Koji Kondo",
+    BPM=180,
+    StepsPerBeat=48,
+    Complete=true,
+    Source="User-supplied MIDI",
+    SourceLicense="User-provided source file; arrangement converted for Velora.",
+    Categories={"Famous","Game OST","Super Mario","Nintendo","Upbeat","Piano","Complete"},
+    Notes=table.concat(sheet, " "),
+}
