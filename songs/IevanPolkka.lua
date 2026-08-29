b53628f400c5de2324a070f46144670bfa7e293c
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-08-29 18:32:41 +0400

    Fix Ievan Polkka with accurate traditional melody

--- songs/IevanPolkka.lua
+++ songs/IevanPolkka.lua
@@ -1,42 +1,53 @@
--- Velora native arrangement of the traditional Finnish folk tune "Ievan Polkka".
--- Self-contained: no runtime song fetches or external arrangement dependency.
--- Arrangement created for Velora's Roblox piano mapping.
+-- Velora native transcription of the traditional Finnish tune "Ievan Polkka".
+-- Melody source: Traditional Tune Archive / Henrik Norbeck traditional transcription.
+-- Converted to Velora's 61-key Roblox piano mapping in B minor.
+-- Structure: AABB, repeated twice as an instrumental performance.
 
 return {
     Id="ievan-polkka",
     Name="Ievan Polkka",
     Artist="Traditional Finnish",
-    BPM=119,
-    StepsPerBeat=8,
+    BPM=132,
+    StepsPerBeat=4,
     Complete=true,
-    Source="Traditional Finnish folk melody; original Velora arrangement",
-    SourceLicense="Public-domain traditional melody; Velora arrangement released with this repository.",
-    Categories={"Famous","TikTok","Folk","Upbeat","Piano","Complete"},
+    DurationSeconds=58.182,
+    OriginalDurationSeconds=58.182,
+    TempoEvents=1,
+    TimeSignatures={"2/4"},
+    Source="https://ttadev.org/wiki/Ievan_Polkka",
+    SourceLicense="Traditional melody; source transcription available under CC BY-SA unless otherwise noted.",
+    Categories={"Famous","TikTok","Folk","Polka","Upbeat","Piano","Complete"},
     Notes=[=[
-[7r] - - - Q - - - [7y] - - - Q - - - [7r] - - - Q - - - [7y] - - - Q - - - [a7] - - - a - - - [aQ] - - - S - - - [d7] - - - a - - - [aQ] - - - d - - -
-[S6] - - - p - - - [p0] - - - S - - - [d7] - - - S - - - [aQ] - - - [pI] - - - [a7] - - - a - - - [aQ] - - - S - - - [d7] - - - a - - - [aQ] - - - - - - -
-[f6] - - - G - - - [f0] - - - d - - - [S7] - - - a - - - Q - - - - - - - [a7] - - - a - - - [aQ] - - - S - - - [d7] - - - a - - - [aQ] - - - d - - -
-[S6] - - - p - - - [p0] - - - S - - - [d7] - - - S - - - [aQ] - - - [pI] - - - [a7] - - - a - - - [aQ] - - - S - - - [d7] - - - a - - - [aQ] - - - - - - -
-[f6] - - - G - - - [f0] - - - d - - - [S7] - - - a - - - Q - - - - - - - [G7] - - - d - - - [dQ] - - - G - - - [f6] - - - S - - - [S0] - - - d - - -
-[f6] - - - G - - - [f0] - - - d - - - [a7] - - - S - - - [dQ] - - - f - - - [G7] - - - d - - - [dQ] - - - G - - - [f6] - - - S - - - [S0] - - - d - - -
-[f6] - - - G - - - [f0] - - - d - - - [S7] - - - a - - - Q - - - - - - - [G7] - - - d - - - [dQ] - - - G - - - [f6] - - - S - - - [S0] - - - d - - -
-[f6] - - - G - - - [f0] - - - d - - - [a7] - - - S - - - [dQ] - - - f - - - [G7] - - - d - - - [dQ] - - - G - - - [f6] - - - S - - - [S0] - - - d - - -
-[f6] - - - G - - - [f0] - - - d - - - [S7] - - - a - - - Q - - - - - - - [a7r] - - - a - - - [aQy] - - - S - - - [d7r] - - - a - - - [aQy] - - - d - - -
-[S6e] - - - p - - - [p0T] - - - S - - - [d7r] - - - S - - - [aQy] - - - [pI] - - - [a7r] - - - a - - - [aQy] - - - S - - - [d7r] - - - a - - - [aQy] - - - - - - -
-[f6e] - - - G - - - [f0T] - - - d - - - [S7r] - - - a - - - [Qy] - - - - - - - [a7r] - - - a - - - [aQy] - - - S - - - [d7r] - - - a - - - [aQy] - - - d - - -
-[S6e] - - - p - - - [p0T] - - - S - - - [d7r] - - - S - - - [aQy] - - - [pI] - - - [a7r] - - - a - - - [aQy] - - - S - - - [d7r] - - - a - - - [aQy] - - - - - - -
-[f6e] - - - G - - - [f0T] - - - d - - - [S7r] - - - a - - - [Qy] - - - - - - - [G7r] - - - d - - - [dQy] - - - G - - - [f6e] - - - S - - - [S0T] - - - d - - -
-[f6e] - - - G - - - [f0T] - - - d - - - [a7r] - - - S - - - [dQy] - - - f - - - [G7r] - - - d - - - [dQy] - - - G - - - [f6e] - - - S - - - [S0T] - - - d - - -
-[f6e] - - - G - - - [f0T] - - - d - - - [S7r] - - - a - - - [Qy] - - - - - - - [G7r] - - - d - - - [dQy] - - - G - - - [f6e] - - - S - - - [S0T] - - - d - - -
-[f6e] - - - G - - - [f0T] - - - d - - - [a7r] - - - S - - - [dQy] - - - f - - - [G7r] - - - d - - - [dQy] - - - G - - - [f6e] - - - S - - - [S0T] - - - d - - -
-[f6e] - - - G - - - [f0T] - - - d - - - [S7r] - - - a - - - [Qy] - - - - - - - [a7r] - - - [aQ] - - - [aQy] - - - [Sr] - - - [d7r] - - - [aQ] - - - [aQy] - - - [dr] - - -
-[S6e] - - - [p0] - - - [p0T] - - - [Se] - - - [d7r] - - - [SQ] - - - [aQy] - - - [prI] - - - [a7r] - - - [aQ] - - - [aQy] - - - [Sr] - - - [d7r] - - - [aQ] - - - [aQy] - - - r - - -
-[f6e] - - - [G0] - - - [f0T] - - - [de] - - - [S7r] - - - [aQ] - - - [Qy] - - - r - - - [a7r] - - - [aQ] - - - [aQy] - - - [Sr] - - - [d7r] - - - [aQ] - - - [aQy] - - - [dr] - - -
-[S6e] - - - [p0] - - - [p0T] - - - [Se] - - - [d7r] - - - [SQ] - - - [aQy] - - - [prI] - - - [a7r] - - - [aQ] - - - [aQy] - - - [Sr] - - - [d7r] - - - [aQ] - - - [aQy] - - - r - - -
-[f6e] - - - [G0] - - - [f0T] - - - [de] - - - [S7r] - - - [aQ] - - - [Qy] - - - r - - - [G7r] - - - [dQ] - - - [dQy] - - - [Gr] - - - [f6e] - - - [S0] - - - [S0T] - - - [de] - - -
-[f6e] - - - [G0] - - - [f0T] - - - [de] - - - [a7r] - - - [SQ] - - - [dQy] - - - [fr] - - - [G7r] - - - [dQ] - - - [dQy] - - - [Gr] - - - [f6e] - - - [S0] - - - [S0T] - - - [de] - - -
-[f6e] - - - [G0] - - - [f0T] - - - [de] - - - [S7r] - - - [aQ] - - - [Qy] - - - r - - - [G7r] - - - [dQ] - - - [dQy] - - - [Gr] - - - [f6e] - - - [S0] - - - [S0T] - - - [de] - - -
-[f6e] - - - [G0] - - - [f0T] - - - [de] - - - [a7r] - - - [SQ] - - - [dQy] - - - [fr] - - - [G7r] - - - [dQ] - - - [dQy] - - - [Gr] - - - [f6e] - - - [S0] - - - [S0T] - - - [de] - - -
-[f6e] - - - [G0] - - - [f0T] - - - [de] - - - [S7r] - - - [aQ] - - - [Qy] - - - r - - - [7Qrad] - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- A1
+a - - a a - S - | d - a - a - d - | S - p - p - S - | d S a p a - I -
+a - - a a - S - | d - a - a - - - | f - G f d - S - | a - - - a - - -
+
+-- A2
+a - - a a - S - | d - a - a - d - | S - p - p - S - | d S a p a - I -
+a - - a a - S - | d - a - a - - - | f - G f d - S - | a - - - a - - -
+
+-- B1
+G - - d d - G - | f - S - S - d - | f - G f d - S - | a - S - d - f -
+G - - d d - G - | f - S - S - d - | f - G f d - S - | a - - - a - - -
+
+-- B2
+G - - d d - G - | f - S - S - d - | f - G f d - S - | a - S - d - f -
+G - - d d - G - | f - S - S - d - | f - G f d - S - | a - - - a - - -
+
+-- A1 repeat
+a - - a a - S - | d - a - a - d - | S - p - p - S - | d S a p a - I -
+a - - a a - S - | d - a - a - - - | f - G f d - S - | a - - - a - - -
+
+-- A2 repeat
+a - - a a - S - | d - a - a - d - | S - p - p - S - | d S a p a - I -
+a - - a a - S - | d - a - a - - - | f - G f d - S - | a - - - a - - -
+
+-- B1 repeat
+G - - d d - G - | f - S - S - d - | f - G f d - S - | a - S - d - f -
+G - - d d - G - | f - S - S - d - | f - G f d - S - | a - - - a - - -
+
+-- B2 repeat
+G - - d d - G - | f - S - S - d - | f - G f d - S - | a - S - d - f -
+G - - d d - G - | f - S - S - d - | f - G f d - S - | a - - - a - - -
 ]=],
 }
