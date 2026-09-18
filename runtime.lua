45aa4e846b67400773155ed9f6395db1c15e9f2c
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-18 07:19:03 +0400

    Refresh Velora song cache for Hedwig's Theme

--- runtime.lua
+++ runtime.lua
@@ -106,7 +106,7 @@ releaseSource = replaceOnce(
 releaseSource = replaceOnce(
     releaseSource,
     'AssetRevision = "0.10.21-seekfix1"',
-    'AssetRevision = "0.10.29-kefik-1"',
+    'AssetRevision = "0.10.30-hedwig-1"',
     "song library revision"
 )
 
