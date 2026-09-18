d654ac1fcd5b5d2a2e5c81d0cb1b624a53e39595
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-18 02:47:49 +0000

    Register Fadel Chaker - Kefik 3ala Fraqe

--- runtime.lua
+++ runtime.lua
@@ -106,7 +106,7 @@ releaseSource = replaceOnce(
 releaseSource = replaceOnce(
     releaseSource,
     'AssetRevision = "0.10.21-seekfix1"',
-    'AssetRevision = "0.10.28-arabic-classics-1"',
+    'AssetRevision = "0.10.29-kefik-1"',
     "song library revision"
 )
 
