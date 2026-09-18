89a937323312af8421a9d13d07dd5b0624426d82
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-18 06:43:28 +0400

    Add Kalimat piano arrangement

---
+++ songs/Kalimat.lua
@@ -0,0 +1,105 @@
+-- Velora 0.10.28 Nova — complete arrangement converted from a MIDI supplied by the repository owner.
+-- Supplied file: kalimat-yusuf0220.mid
+-- Supplied file SHA-256: 9512CD6179F5CA0B40055A785B114DED178C5B0179FBDA28C7AE0DB9B12AB638
+-- MIDI: 480 PPQ, 1 source tempo event, source time signature(s): 4/4.
+-- Timing normalized to real seconds at 100 BPM / 48 Velora steps per beat, preserving all source tempo changes.
+-- 4 non-drum source tracks retained; simultaneous duplicate pitches are merged.
+-- 112 out-of-range note-on events were folded by octave into Velora's C2-C7 keyboard range.
+-- 3583 source note-ons became 3241 mapped notes after same-step duplicate merging.
+
+local DATA = [=[
+0:[^P] 23:[yd] 23:[pj] 23:[PJ] 23:[2odh] 47:[ig] 47:[1YosD] 71:[1ig] 7:[YD] 7:[yd] 7:[1YD] 47:D 47:[4psg] 23:[ts] 23:[oh] 23:[4pj] 23:[4ig] 47:[YD] 47:[^yPd] 71:[^YD] 7:[yd] 7:[ts]
+7:[6ypd] 95:[5oP] 23:[yd] 23:[sl] 23:[5dz] 23:[5yioak] 23:[Yl] 23:[ioz] 23:[oaZ] 23:[1Yosl] 71:[1iz] 7:[Yl] 7:[yk] 7:[1Yl] 47:[^J] 47:[4g] 23:[yg] 23:[yJ] 23:[4Yl] 23:[4tDj]
+23:[yJ] 23:[Yl] 23:[iz] 23:[^ydJ] 71:[^Yl] 7:[yJ] 7:[tj] 7:[^yJ] 47:[6j] 23:[PJ] 11:[pj] 11:[5odh] 23:[dz] 47:[5pj] 11:[oh] 11:[5oh] 71:[pj] 11:[oh] 11:[4isg] 23:[sl] 47:[4pj]
+11:[oh] 11:[4ig] 71:[ig] 11:[YD] 11:[@yoPdD] 23:[YD] 23:[ig] 23:[@oh] 23:[2ipdj] 23:[PJ] 23:[sl] 23:[dz] 23:[4ipDgZ] 23:[sl] 47:J 23:[^iPz] 71:J 11:j 11:[2Idl] 71:j 11:h 11:[@oDJ]
+71:[hv] 11:[gc] 11:[1sDZ] 23:[jb] 23:[gc] 23:[DZ] 23:[2dz] 59:[pj] 11:[PJ] 11:[sl] 11:[1sdz] 23:[jb] 23:[gc] 23:[DZ] 23:[2dz] 59:[pj] 11:[PJ] 11:[sl] 11:[@dDz] 23:[jb] 23:[gc]
+23:[DZ] 23:[^Pdz] 71:[6psl] 23:[5oPJ] 23:[dz] 47:[dz] 23:[5dhv] 71:[gc] 23:[1ts] 23:D 47:D 11:d 11:[1g] 47:D 23:[^Pd] 11:D 11:[4is] 23:[sl] 23:s 23:[sl] 23:[4gc] 47:[DZ] 47:[^yP]
+23:[dz] 47:[sl] 11:[PJ] 11:[^dz] 47:[6p] 23:[DZ] 7:[dz] 7:[sl] 7:[5odz] 23:[dz] 47:[dz] 11:d 11:[5dhv] 71:[gc] 23:[1ts] 23:[DZ] 47:[DZ] 11:[dz] 11:[1gc] 47:[DZ] 23:[^Pd] 11:D
+11:[4sg] 23:[sl] 11:s 11:s 23:[sl] 23:[4gc] 47:[DZ] 47:[^yP] 23:[dz] 47:[sl] 11:[PJ] 11:[^dz] 47:[4g] 23:[PB] 11:[pb] 11:[@oDv] 23:[PJ] 47:[@PJ] 11:P 11:[@DZ] 71:[dz] 23:[1Ys]
+23:[sl] 23:[sl] 23:[1ak] 23:[1sl] 35:[PJ] 11:[pj] 47:[2Id] 23:[pj] 47:[2PJ] 11:[sl] 11:[2dz] 11:d 11:D 11:h 11:D 23:d 23:[5yo] 11:[dz] 11:[JB] 23:[jb] 23:[5hv] 23:[2djb] 23:[gc]
+23:[2hv] 23:[DZ] 23:[5ydhz] 23:[dz] 47:[dz] 11:d 11:[5yhv] 71:[gc] 23:[1ts] 23:[DZ] 47:[DZ] 11:[dz] 11:[1gc] 47:[DZ] 23:[^Pd] 11:D 11:[4is] 23:[sl] 11:[sl] 11:[sl] 23:[sl] 23:[4gc]
+47:[DZ] 47:[^yP] 23:[dz] 47:[sl] 11:[PJ] 11:[^dz] 47:[4g] 23:[PB] 11:[pb] 11:[@oDv] 23:[PJ] 47:[@PJ] 11:[PJ] 11:[@DZ] 71:[dz] 23:[1Ys] 23:[sl] 23:[sl] 23:[1ak] 23:[6psl] 35:[PJ]
+11:[pj] 47:[2Id] 23:[pj] 47:[2PJ] 11:[sl] 11:[2dz] 11:d 11:D 11:h 11:D 23:d 23:[@oD] 11:[dz] 11:[JB] 23:[jb] 23:[@hv] 23:[2Idjb] 47:2 23:j 23:[@DJ] 23:[JB] 23:[@PDJB] 23:[JB]
+23:[@PDJB] 23:[JB] 23:[@PDhv] 23:[jb] 23:[2pdjb] 23:j 23:[2pdj] 11:g 11:[^D] 11:d 11:[2pdj] 11:d 11:D 11:d 11:[2pdj] 11:g 11:D 11:d 11:[2pd] 23:[jb] 11:j 11:[2pdjb] 47:[2pd]
+23:[jb] 11:j 11:[2pdjb] 23:[JB] 23:[@PD] 23:[hv] 23:[@PDgc] 23:[hv] 23:[@oD] 5:p 5:P 5:s 5:d 5:D 5:g 5:h 5:[@Dj] 5:J 5:l 5:z 5:[jZb] 5:c 5:v 5:b 5:[@DJ] 23:[JB] 23:[@PDJB] 11:J
+11:[JB] 23:[@PDJB] 23:[JB] 23:[@PDhv] 23:[jb] 23:[2pdjb] 23:j 23:[2pdj] 11:g 11:[^D] 11:d 11:[2pdj] 11:d 11:D 11:d 11:[2pdj] 11:g 11:D 11:d 11:[2pd] 23:[jb] 11:[jb] 11:[2pdjb]
+47:[2pd] 23:[jb] 11:[jb] 11:[2pdjb] 23:[JB] 23:[@PD] 23:[hv] 23:[@PDgc] 23:[hv] 23:[@D] 5:E 5:t 5:y 5:Y 5:i 5:o 5:p 5:[@PD] 5:s 5:d 5:D 5:[ghv] 5:h 5:j 5:J 5:[1slm] 71:[1kn]
+23:[1oPhlm] 95:[2pdG] 23:[kn] 47:[2z] 11:[lm] 11:[@DhJB] 23:h 11:j 11:J 23:[jb] 11:[JB] 11:[1oslm] 23:[JB] 23:[jb] 23:[hv] 23:[@DJB] 23:[jb] 23:[hv] 23:[gc] 23:[2idjb] 23:[hv]
+23:[gc] 23:[DZ] 23:[@oDhv] 71:[DZ] 11:[DZ] 11:[6YpDZ] 47:[pj] 47:[4iPJ] 47:[sl] 47:[^yPgc] 23:[DZ] 23:[DZ] 23:[dz] 23:[2Idz] 95:[5oP] 23:[yz] 23:[pb] 23:[5PB] 23:[2odv] 47:[ic]
+47:[1YosZ] 71:[1ic] 7:[YZ] 7:[yz] 7:[1YZ] 47:D 47:[4isg] 23:[tl] 23:[ov] 23:[4pb] 23:[4ic] 47:[YZ] 47:[^yPz] 71:[^YZ] 7:[yz] 7:[tl] 7:[6ypz] 95:[5oP] 23:[yz] 23:[sm] 23:[5dz]
+23:[5yioan] 23:[Ym] 23:[iz] 23:[oZ] 23:[1Yism] 71:[1iz] 7:[Ym] 7:[yn] 7:[1Ysm] 47:[^P] 47:[4g] 23:[yc] 23:[yB] 23:[4Ym] 23:[4tDb] 23:[yB] 23:[Ym] 23:[iz] 23:[^yPdB] 71:[^Ym] 7:[yB]
+7:[tb] 7:[^yJB] 47:[6j] 23:[PB] 11:[pb] 11:[5odhv] 23:[dz] 47:[5pb] 11:[ov] 11:[5ov] 71:[pb] 11:[ov] 11:[4ipsgc] 23:[sm] 47:[4pb] 11:[ov] 11:[4ic] 71:[ig] 11:[YD] 11:[@yoPdD]
+23:[YD] 23:[ig] 23:[@oh] 23:[2ipdj] 23:[PJ] 23:[sl] 23:[dz] 23:[4ipDgZ] 23:[sl] 47:J 23:[^iPz] 71:J 11:j 11:[2Idl] 71:j 11:h 11:[@oDJ] 71:[hv] 11:[gc] 11:[1sDZ] 23:[jb] 23:[gc]
+23:[DZ] 23:[^Pdz] 59:[pj] 11:[PJ] 11:[sl] 11:[1sdz] 23:[jb] 23:[gc] 23:[DZ] 23:[2dz] 59:[pj] 11:[PJ] 11:[sl] 11:[@dDz] 23:[jb] 23:[gc] 23:[DZ] 23:[^Pdz] 71:[6psl] 23:[5oPJ] 23:[dz]
+47:[dz] 11:d 11:[5dhv] 71:[gc] 23:[1ts] 23:[DZ] 47:[DZ] 11:[dz] 11:[1gc] 47:[DZ] 23:[^Pd] 11:D 11:[4is] 23:[sl] 11:[sl] 11:[sl] 23:[sl] 23:[4gc] 47:[DZ] 47:[^yP] 23:[dz] 47:[sl]
+11:[PJ] 11:[^dz] 47:[4g] 23:[PB] 11:[pb] 11:[@oDv] 23:[PJ] 47:[@PJ] 11:[PJ] 11:[@DZ] 71:[dz] 23:[1Ys] 23:[sl] 23:[sl] 23:[1ak] 23:[1sl] 35:[PJ] 11:[pj] 47:[2Id] 23:[pj] 47:[2PJ]
+11:[sl] 11:[2dz] 11:d 11:D 11:h 11:D 23:d 23:[@oD] 11:[dz] 11:[JB] 23:[jb] 23:[@hv] 23:[2idjb] 47:2 23:j 23:[@DJ] 23:[JB] 23:[@PDJB] 11:[JB] 11:[JB] 23:[@PDJB] 23:[JB] 23:[@PDhv]
+23:[jb] 23:[2pdjb] 23:j 23:[2pdj] 11:g 11:[^D] 11:d 11:[2pdj] 11:d 11:D 11:d 11:[2pdj] 11:g 11:D 11:d 11:[2pd] 23:[jb] 11:j 11:[2pdjb] 47:[2pd] 23:[jb] 11:j 11:[2pdjb] 23:[JB]
+23:[@PD] 23:[hv] 23:[@PDgc] 23:[hv] 23:[@D] 23:y 5:Y 5:i 5:o 5:[@pD] 5:P 5:s 5:d 5:[Djb] 5:g 5:h 5:j 5:[@DJ] 23:[JB] 23:[@PDJB] 11:J 11:[JB] 23:[@PDJB] 23:[JB] 23:[@PDhv] 23:[jb]
+23:[2pdjb] 23:j 23:[2pdj] 11:g 11:[^D] 11:d 11:[2pdj] 11:d 11:D 11:d 11:[2pdj] 11:g 11:D 11:d 11:[2pd] 23:[jb] 11:[jb] 11:[2pdjb] 47:[2pd] 23:[jb] 11:[jb] 11:[2pdjb] 23:[JB]
+23:[@PD] 23:[hv] 23:[@PDgc] 23:[hv] 23:[@D] 23:Y 5:i 5:o 5:p 5:[@PD] 5:s 5:d 5:D 5:[ghv] 5:h 5:j 5:J 5:[1slm] 71:[1kn] 23:[1oPhlm] 95:[2pdG] 23:[kn] 47:[2z] 11:[lm] 11:[@DhJB] 23:h
+11:j 11:J 23:[jb] 11:[JB] 11:[1oslm] 23:[JB] 23:[jb] 23:[hv] 23:[@DJB] 23:[jb] 23:[hv] 23:[gc] 23:[2idjb] 23:[hv] 23:[gc] 23:[DZ] 23:[@oDhv] 71:[DZ] 11:[DZ] 11:[6YpDZ] 47:[pj]
+47:[4iPJ] 47:[sl] 47:[^yPgc] 23:[DZ] 23:[DZ] 23:[dz] 23:[^dz] 23:h 11:j 11:J 23:[JB] 11:[lm] 11:[2oPdz] 47:[lm] 23:[2JB] 23:[@IDlm] 47:[JB] 23:[jb] 23:[4isgJB] 47:[hv] 23:[jb]
+23:[^PJB] 71:[JB] 11:[lm] 11:[@oDz] 47:[lm] 23:[@JB] 23:[4pglm] 47:[JB] 23:[jb] 23:[^iPlm] 47:[lm] 23:[JB] 23:[^tsJ] 11:[yd] 11:[YD] 11:[ig] 11:[oh] 11:[pj] 11:[PJ] 11:[sl]
+11:[5oPz] 11:[pl] 11:[Pz] 11:[oJ] 23:[oh] 11:[5opj] 11:[PJ] 11:[5oPz] 11:[pl] 11:[Pz] 11:[oJ] 23:[oh] 11:[5opj] 11:[PJ] 11:[5oPz] 11:[pl] 11:[Pz] 11:[oJ] 23:[oh] 11:[5opj] 11:[PJ]
+11:[1sZ] 11:[Pz] 11:[sZ] 23:[sl] 23:[1s] 23:[1sZ] 11:[Pz] 11:[sZ] 11:[pl] 23:[pj] 11:[1PsJ] 11:[sl] 11:[1sZ] 11:[Pz] 11:[sZ] 11:[pl] 23:[pj] 11:[1PsJ] 11:[sl] 11:[1sZ] 11:[Pz]
+11:[sZ] 11:[pl] 11:[4sdgc] 11:[sx] 11:[dc] 11:[Pz] 11:[@Dv] 11:[dc] 11:[sZ] 11:[dc] 11:[5osZ] 23:[Pz] 23:[5oPz] 11:[pl] 11:[Pz] 11:[oJ] 23:[oh] 11:[5opj] 11:[PJ] 11:[5oPz] 11:[pl]
+11:[Pz] 11:[oJ] 23:[oh] 11:[5opj] 11:[PJ] 11:[5oPz] 11:[pl] 11:[Pz] 11:[oJ] 23:[oh] 11:[5opj] 11:[PJ] 11:[1sZ] 11:[Pz] 11:[sZ] 23:[sl] 23:[1s] 23:[1sZ] 11:[Pz] 11:[sZ] 11:[pl]
+23:[pj] 11:[1PsJ] 11:[sl] 11:[1sZ] 11:[Pz] 11:[sZ] 11:[pl] 23:[pj] 11:[1PsJ] 11:[sl] 11:[1sZ] 11:[Pz] 11:[sZ] 11:[pl] 11:[4sdgc] 11:[sx] 11:[dc] 11:[Pz] 11:[@Dv] 11:[dc] 11:[sZ]
+11:[dc] 11:[5osZ] 23:[Pz] 23:[4tisl] 11:[PJ] 11:[pj] 11:[^yPJ] 59:[@oD] 11:i 11:Y 11:y 11:[1Ys] 47:[4tisl] 11:[PJ] 11:[pj] 11:[^yPJ] 59:[@Dg] 5:D 5:d 5:s 5:D 5:d 5:s 5:P 5:[4pg]
+47:[4tisl] 11:[PJ] 11:[pj] 11:[^yPJ] 59:[@oD] 11:i 11:Y 11:y 11:[1Ys] 47:[4tisl] 11:[PJ] 11:[pj] 11:[^yPJ] 59:[@Dg] 5:D 5:d 5:s 5:D 5:d 5:s 5:P 5:[4pg] 47:[2yd] 11:[ts] 11:[yd]
+11:[ts] 11:[yd] 11:[ts] 11:[2yd] 23:[5h] 23:[hv] 23:[JB] 23:5 23:[2yd] 11:[ts] 11:[yd] 11:[ts] 11:[yd] 11:[ts] 11:[2yd] 23:[2pdb] 11:[pb] 11:[PB] 11:[PB] 11:[sm] 11:[sm] 11:[dz]
+23:[5oPhv] 11:[hv] 11:[hv] 11:h 11:[hv] 23:[hJB] 23:[5hJ] 23:[jb] 23:[hjv] 23:[hv] 23:[2dh] 23:[GC] 23:[GJB] 11:[JB] 11:[JB] 11:J 11:[5hJ] 95:[2edGjb] 23:[jb] 23:[jb] 23:j
+23:[@(Dhjb] 6:q 6:( 6:q 2:[jlm] 2:( 6:q 6:( 3:g 2:[lm] 2:( 5:g 6:( 7:[qJlB] 1:( 9:q 5:( 5:[1sDhJ] 23:[jb] 23:[hjv] 23:[hjb] 23:[2dGj] 95:[5dhv] 11:[hv] 11:[hv] 11:h 11:[hv]
+23:[hJB] 23:[5hJ] 23:[jb] 23:[hjv] 23:[hv] 23:[2dh] 23:[GC] 23:[GJB] 11:[JB] 11:[JB] 11:J 11:[5hJ] 95:[2dG] 95:[@Dh] 44:[qg] 11:[qg] 38:[1sDh] 95:[2dG] 95:[5dhv] 23:h 47:[JB]
+23:[5hJ] 23:[jb] 15:[hv] 7:j 7:[GC] 7:h 7:[DZ] 7:G 15:[5dDz] 23:[dJB] 23:[jJb] 23:[hjv] 23:[5h] 47:[hv] 23:[hJB] 23:[2dGjJ] 23:[jb] 23:[hjv] 23:h 23:[1sDJlm] 23:l 11:[JB] 11:[jb]
+11:[hJv] 11:[jb] 11:h 11:[6pj] 23:Z 23:[lZm] 23:[lz] 23:[2dGjz] 47:2 47:[5odz] 23:[dz] 23:[dz] 23:[dz] 23:[5oz] 11:z 23:[oz] 59:[2pdz] 23:[dz] 23:[dz] 23:[dlm] 23:[5dl] 11:[lm]
+23:[dl] 59:[5odz] 23:[dz] 23:[dz] 23:[dz] 23:[5oz] 11:z 23:[oz] 59:[2pdz] 23:[dz] 23:[dz] 23:[dlm] 23:[5dl] 11:[lm] 23:[dl] 59:[@odDhv] 11:[jb] 11:[hJB] 11:j 11:J 23:[hv]
+23:[4psgh] 23:[jb] 23:j 23:[gc] 23:[^Pdg] 23:[jb] 23:j 47:[2pd] 95:[5oPdz] 23:[dz] 23:[dz] 23:[dz] 23:[5oz] 23:z 23:z 23:[lm] 23:[2dl] 71:[lm] 23:[2dl] 23:[JB] 23:[@DJ] 47:[@odDhv]
+11:[jb] 11:[hJB] 11:j 11:J 23:[hv] 23:[4psgh] 23:[jb] 23:j 23:[jb] 23:[2pdj] 95:4 71:[sl] 11:[DZ] 11:[^Pdz] 23:z 23:[dz] 23:[sl] 23:[5oslv] 47:[ak] 47:[4iOHc] 71:[ak] 23:[%Osl]
+23:[OH] 23:[ig] 47:[4sg] 23:H 47:g 23:[@EDZ] 23:D 23:h 47:[2ydz] 47:d 23:s 23:[tdl] 11:[yz] 11:[YZ] 11:[IC] 11:[ov] 11:[pb] 11:[PB] 11:[sm] 11:[2dz] 23:z 23:[dz] 23:[sl] 23:[5oslv]
+47:[ak] 47:[4iOHc] 71:[ak] 23:[%Osl] 23:[OH] 23:[ig] 47:[4ig] 23:[OH] 47:[ig] 23:[@DZ] 23:[YD] 23:[oh] 47:[2ydz] 47:d 23:s 23:d 23:[yz] 11:[YZ] 11:[yz] 23:d 11:D 11:[2yh] 23:[5yh]
+23:[2yh] 23:[5yh] 23:[2yh] 23:[5yh] 23:[2yh] 23:[5yh] 23:[2yh] 23:[5yh] 23:[2yh] 23:[5yh] 23:[2Id] 23:[dGC] 11:[Dhv] 11:[2Gjb] 47:[2Idj] 23:[$Id] 23:[2Idl] 23:[$Id] 23:[2IdZ]
+23:[$Id] 23:[2Idz] 23:[$Idl] 23:[2Idz] 23:[$Id] 23:[2IdZ] 23:[$Id] 23:[5yhJz] 11:[jl] 11:[hJ] 11:[Gj] 11:[5h] 11:G 11:D 11:d 11:[2yh] 23:[5yh] 23:[2yh] 23:[5yh] 23:[2yh] 23:[5yh]
+23:[2yh] 23:[5yh] 23:[2yh] 23:[5yh] 23:[2yh] 23:[5yh] 23:[2Id] 23:[dGC] 11:[Dhv] 11:[2Gjb] 47:[2Idj] 23:[$Id] 23:[2Idl] 23:[$Id] 23:[2IdZ] 23:[$Id] 23:[2Idz] 23:[$Idl] 23:[2IdJz]
+23:[$Id] 23:[2IdlZ] 23:[$Id] 23:[2Idjz] 71:Z 11:Z 11:[1oshZ] 47:[oZ] 47:[4isghj] 71:z 11:z 11:[^iPgz] 47:[iz] 47:[@YPDgh] 71:g 11:h 11:[2idj] 11:J 11:j 11:J 11:h 11:j 11:h 11:j
+11:[@oDJ] 11:l 11:J 11:l 11:j 11:J 11:j 11:J 11:[4pgl] 71:Z 23:[^PZ] 47:z 47:[^Pg] 23:[^P] 23:[^P] 23:[^P] 23:[^P] 23:[^P] 23:[^P] 23:[JB] 11:[lm] 11:[@oDz] 47:[lm] 23:[@JB]
+23:[@IDlm] 47:[JB] 23:[jb] 23:[4igJB] 47:[hv] 23:[jb] 23:[^PJB] 71:[JB] 11:[lm] 11:[@oDz] 47:[lm] 23:[@JB] 23:[@IDlm] 47:[JB] 23:[jb] 23:[4igJB] 47:[hv] 23:[jb] 23:[^PJB] 71:[JB]
+11:[lm] 11:[@oDz] 47:[lm] 23:[@JB] 23:[@IDlm] 47:[JB] 23:[jb] 23:[4igJB] 47:[hv] 23:[jb] 23:[^PJB] 47:[dz] 23:[sm] 23:[2pdGz] 23:z 23:z 23:z 11:Z 11:[@oPDhJz] 71:l 23:[1sz] 47:l
+23:J 11:l 11:[2pdJ] 47:j 47:[2pdjz] 23:[2pd] 23:[2pd] 23:[2pd] 23:[2pdjz] 23:[2pd] 23:[2pd] 23:[2pd] 23:[@PdDhz] 23:[@PD] 23:[@PD] 23:[@PDz] 11:z 11:[2pdjz] 23:[Ylm] 11:[IJB]
+11:[2opjb] 11:[phv] 11:[PGC] 11:[sDZ] 11:[2pdj]
+]=]
+local TRAILING_RESTS = 45
+
+local sheet = {}
+for encoded in DATA:gmatch("%S+") do
+    local gap, token = encoded:match("^(%d+):(.+)$")
+    gap = tonumber(gap)
+    if gap and token then
+        for _ = 1, gap do
+            sheet[#sheet + 1] = "-"
+        end
+        sheet[#sheet + 1] = token
+    end
+end
+for _ = 1, TRAILING_RESTS do
+    sheet[#sheet + 1] = "-"
+end
+
+return {
+    Id="kalimat-majida-el-roumi",
+    Name="Kalimat",
+    Artist="Majida El Roumi",
+    BPM=100,
+    StepsPerBeat=48,
+    Complete=true,
+    DurationSeconds=382.170,
+    OriginalDurationSeconds=382.170,
+    TempoEvents=1,
+    TimeSignatures={"4/4"},
+    Source="User-supplied MIDI",
+    SourceFile="kalimat-yusuf0220.mid",
+    SourceLicense="User-provided source file; converted for Velora at the repository owner's request.",
+    Categories={"Famous","Arabic","Lebanese","Majida El Roumi","Classic","Romantic","Piano","Complete"},
+    Notes=table.concat(sheet, " "),
+}
