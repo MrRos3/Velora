d4dc317156d6b6606bfe0807f0928cd8a679baea
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-19 12:55:39 +0400

    Add Conquest of Paradise MIDI arrangement

---
+++ songs/ConquestOfParadise.lua
@@ -0,0 +1,97 @@
+-- Velora 0.10.34 Nova — complete arrangement converted from a MIDI supplied by the repository owner.
+-- Source file: conquest-of-paradise-vangelis-arrangement-by-patrik-pietschmann.mid
+-- MIDI: 480 PPQ, 12 source tempo event(s), source time signature(s): 3/4.
+-- 2 non-drum musical track(s) retained; simultaneous duplicate pitches are merged.
+-- 189 out-of-range note-on events were octave-folded into Velora's C2-C7 keyboard range.
+-- 3,264 source note-ons became 3,106 mapped notes after same-step duplicate merging.
+
+local DATA = [=[
+0:[29ey] 23:[29ey] 11:[29ey] 11:[29ey] 23:[29ey] 11:[29ey] 11:[29ey] 23:[29ey] 23:[29ey] 23:[29ey] 11:[29ey] 11:[29ey] 23:[29ey] 11:[29ey] 11:[29ey] 23:[29ey] 23:[29ey] 23:[29ey] 11:[29ey] 11:[29ey] 23:[29ey] 11:[29ey] 11:[29ey] 23:[29ey] 23:[29ey] 23:[29ey] 11:[29ey] 11:[29ey]
+23:[29ey] 11:[29ey] 11:[29ey] 23:[2e] 23:[29i] 11:e 11:[29q] 11:[29u] 11:[29y] 11:e 11:[29q] 11:[29e] 11:[29T] 11:e 11:[29q] 11:y 11:[6u] 11:e 11:[60] 11:[6T] 11:[6e] 11:0 11:[6*] 11:[60] 11:[6*] 11:0 11:[6e] 11:0
+11:[29i] 11:e 11:[29q] 11:[29u] 11:[29y] 11:e 11:[29q] 11:[29e] 11:[29T] 11:e 11:[29q] 11:y 11:[18u] 11:w 11:[180] 11:[18w] 11:[180] 11:w 11:[180] 11:[18w] 11:[18t] 11:w 11:[180] 11:w 11:[4qp] 11:t 11:[4qe] 11:[4qo]
+11:[4qi] 11:t 11:[4qe] 11:[4qt] 11:[4qu] 11:t 11:[4qe] 11:i 11:[18o] 11:t 11:[18w] 11:[18u] 11:[18t] 11:w 11:[180] 11:[18w] 11:[180] 11:w 11:[180] 11:t 11:[^E] 11:q 11:[^9] 11:[^t] 11:[^y] 11:q 11:[^9] 11:[^q]
+11:[^t] 11:q 11:[^9] 11:E 11:[6e] 11:0 11:[6*] 11:[60] 11:[6*] 11:0 11:[6*] 11:[60] 11:[6*] 11:0 11:[6*] 11:0 11:[6e] 11:0 11:[6*] 11:[60] 11:[6wr] 11:0 11:[6*] 11:[60] 11:[6wT] 23:[6Tup] 23:[2pdg] 11:9
+11:q 11:[epdf] 11:[yipd] 11:e 11:q 11:9 11:[qipS] 11:e 11:y 11:[ipd] 11:[6pSf] 11:6 11:* 11:[0upS] 11:[eTup] 11:0 11:* 11:6 11:* 11:0 11:[eTup] 23:[2pdg] 11:9 11:q 11:[epdf] 11:[yipd] 11:e 11:q
+11:9 11:[qipS] 11:e 11:y 11:[ipd] 11:[1osf] 11:8 11:0 11:w 11:t 11:w 11:0 11:8 11:[0uos] 11:w 11:t 23:[4sgj] 11:q 11:e 11:[tsgh] 11:[ipsg] 11:t 11:e 11:q 11:[epsf] 11:t 11:i 11:[psg]
+11:[1sfh] 11:8 11:0 11:[wosf] 11:[tuos] 11:w 11:0 11:8 11:0 11:w 11:t 11:[uos] 11:[^yiP] 11:^ 11:9 11:[qiPs] 11:[EiPd] 11:q 11:9 11:^ 11:[9iPs] 11:q 11:E 11:[yiP] 11:[6Tup] 11:6 11:* 11:0
+11:e 11:0 11:* 11:6 11:* 11:0 11:[ep] 23:[9g] 11:[ep] 11:[yi] 11:[ef] 11:[ud] 11:[ep] 11:[yi] 11:[ep] 11:[9S] 11:[ep] 11:[yi] 11:[ed] 11:[6f] 11:[0p] 11:[eu] 11:[0S] 11:[rp] 11:[0u] 11:[eT] 11:[0u] 11:[6T]
+11:[0u] 11:[ep] 11:[0u] 11:[9g] 11:[ep] 11:[yi] 11:[ef] 11:[ud] 11:[ep] 11:[yi] 11:[ep] 11:[9S] 11:[ep] 11:[yi] 11:[ed] 11:[8f] 11:[wo] 11:[tu] 11:[wo] 11:[yu] 11:[wo] 11:[tu] 11:[wo] 11:[8s] 11:[wo] 11:[tu] 11:[wo] 11:[qj]
+11:[ts] 11:[ip] 11:[th] 11:[og] 11:[ts] 11:[ip] 11:[ts] 11:[qf] 11:[ts] 11:[ip] 11:[tg] 11:[8h] 11:[ws] 11:[to] 11:[wf] 11:[ys] 11:[wo] 11:[tu] 11:[wo] 11:[8u] 11:[wo] 11:[tu] 11:[ws] 11:[^P] 11:[qi] 11:[Ey] 11:[qs] 11:[td]
+11:[qi] 11:[Ey] 11:[qi] 11:[^s] 11:[qi] 11:[Ey] 11:[qP] 11:[6p] 11:[0u] 11:[eT] 11:[0u] 11:[rT] 11:[0u] 11:[eT] 11:[0u] 11:[6T] 11:[0u] 11:[eT] 11:[0u] 11:[6p] 11:[0u] 11:[eT] 11:[0u] 11:[roa] 11:[0u] 11:[6T] 11:[0u] 11:[eoS]
+11:0 11:[6pSfj] 23:[2gjzc] 23:[eyi] 11:[eyifjzx] 11:[eyidgjz] 23:[eyi] 11:[eyi] 11:[eyiSgjL] 23:[eyi] 11:[dgjz] 11:[6fjLx] 23:[eTu] 11:[eTuSfjL] 11:[eTupSfj] 23:[eTu] 11:[eTu] 11:[eTu] 23:[eTupSfj] 23:[2gjzc] 23:[eyi] 11:[eyifjzx] 11:[eyidgjz] 23:[eyi] 11:[eyi] 11:[eyiSgjL] 23:[eyi] 11:[dgjz]
+11:[1fhlx] 23:[wtu] 11:[wtu] 11:[wtu] 23:[wtu] 11:[wtu] 11:[wtusfhl] 23:[wtu] 23:[4jlcb] 23:[eti] 11:[etihlcv] 11:[etigjlc] 23:[eti] 11:[eti] 11:[etifjlx] 23:[eti] 11:[gjlc] 11:[1hlxv] 23:[wtu] 11:[wtufhlx] 11:[wtusfhl] 23:[wtu] 11:[wtu] 11:[wtu] 23:[wtu] 11:[sfhl] 11:[^PdgJ] 23:[qEy]
+11:[qEysgJl] 11:[qEydgJz] 23:[qEy] 7:[fx] 3:[qEy] 3:[dz] 7:[qEysgJl] 23:[qEy] 11:[PdgJ] 11:[6pSfj] 23:[eTu] 11:[eTu] 11:[eTu] 23:[eTu] 11:[eTu] 11:[eTu] 23:[eTu] 23:[6upS] 11:[6upS] 11:[6upS] 11:[6upS] 11:[6upad] 11:[6upad] 11:[6upad] 11:[6upad] 11:[6upSf] 11:[6upSf] 11:[6upSf]
+11:[6upSf] 11:[ypj] 5:h 5:[eG] 5:d 5:[9p] 5:o 5:[2I] 5:y 5:[2e] 5:w 5:Q 5:9 5:6 5:9 5:Q 5:w 5:[9e] 5:y 5:[eI] 5:o 5:[yp] 5:d 5:[IG] 5:j 5:[oak] 7:[yh] 7:[rd]
+7:[wa] 7:[rd] 7:[yh] 7:[Ipj] 7:[yh] 7:[rd] 7:[wp] 7:[rd] 7:[yh] 7:[uoh] 7:[yd] 7:[ra] 7:[wo] 7:a 7:d 7:[ypj] 5:h 5:[eG] 5:d 5:[9p] 5:o 5:[2I] 5:y 5:[2e] 5:w 5:Q 5:9 5:6
+5:9 5:Q 5:w 5:[9e] 5:y 5:[eI] 5:o 5:[yp] 5:d 5:[IG] 5:j 5:[oak] 7:[yh] 7:[rd] 7:[wa] 7:[rd] 7:[yh] 7:[Ipj] 7:[yh] 7:[rd] 7:[wp] 7:[rd] 7:[yh] 7:[uoh] 7:[yd] 7:[ra] 7:[wo] 7:a
+7:d 7:[ypj] 5:h 5:[eG] 5:d 5:[9p] 5:o 5:[2I] 5:y 5:[2e] 5:w 5:Q 5:9 5:6 5:9 5:Q 5:w 5:[9e] 5:y 5:[6I] 5:o 5:[2p] 5:d 5:G 5:j 5:[5ak] 7:h 7:[9d]
+7:a 7:[wd] 7:h 7:[eSL] 7:k 7:[9h] 7:S 7:[5h] 7:k 7:[rdz] 7:k 7:[9h] 7:d 7:[5a] 7:o 7:[2IpdG] 15:6 15:[9e] 7:y 7:[QI] 7:p 7:[ed] 7:G 7:[yj] 7:z 7:[IC] 7:z 7:[yj]
+7:G 7:[ed] 7:p 7:[6upSf] 15:[6upSf] 15:[6upSf] 15:[6IpSG] 15:[6IpSG] 15:[6IpSG] 15:[6oSfh] 15:[6oSfh] 15:[6oSfh] 15:[2pdGj] 11:6 11:[9e] 5:y 5:[QI] 5:p 5:[ed] 5:G 5:[yj] 5:z 5:[IC] 5:z 5:[yj] 5:G 5:[ez] 5:j
+5:[QG] 5:d 5:[9j] 5:G 5:[6d] 5:p 5:[5ak] 7:h 7:d 7:[wa] 7:[rd] 7:[yh] 7:[Ipj] 7:[yh] 7:[rd] 7:[wp] 7:[rd] 7:[yh] 7:[uoh] 7:[yd] 7:[ra] 7:[wo] 7:a 7:d 7:[2pdGj] 11:6 11:[9e] 5:y
+5:[QI] 5:p 5:[ed] 5:G 5:[yj] 5:z 5:[IC] 5:z 5:[yj] 5:G 5:[ez] 5:j 5:[QG] 5:d 5:[9j] 5:G 5:[6d] 5:p 5:[5adhk] 23:w 11:y 11:[IpdGj] 11:y 11:w 11:y 11:[uoafh] 11:y 11:w
+23:[2pdGj] 11:6 11:[9e] 5:y 5:[QI] 5:p 5:[ed] 5:G 5:[yj] 5:z 5:[IC] 5:v 5:[yC] 5:z 5:[ej] 5:h 5:[QG] 5:d 5:[9p] 11:6 11:[5adhk] 23:w 11:y 11:[pShjL] 11:y 11:w 11:y 11:[adhkz]
+11:y 11:w 23:[6IpdG] 23:[jC] 7:[Gz] 7:[dj] 7:[Gz] 7:[dj] 7:[pG] 7:[dj] 7:[pG] 7:[Id] 7:[pG] 7:[Id] 7:[yp] 7:[Id] 7:[yp] 7:[eI] 7:[6upSf] 15:6 7:0 7:e 7:u 7:p 7:f 7:j 7:x 7:j
+7:x 7:b 23:[pj] 23:[9gc] 11:[ez] 11:[yj] 11:[efx] 11:[idz] 11:[ej] 11:[yg] 11:[ed] 11:[9SL] 11:[ej] 11:[yg] 11:[edz] 11:[*fx] 11:[eL] 11:[Tj] 11:[eSL] 11:[upj] 11:[ef] 11:[TS] 11:[ep] 11:[*S] 11:[ef] 11:[Tpj] 11:[ef] 11:[9gc]
+11:[ez] 11:[yj] 11:[efx] 11:[idz] 11:[ej] 11:[yg] 11:[ed] 11:[9SL] 11:[ej] 11:[yg] 11:[edz] 11:[8fx] 11:[wl] 11:[th] 11:[wf] 11:[uh] 11:[wl] 11:[tx] 11:[wl] 11:[0sl] 11:[th] 11:[of] 11:[ts] 11:[qjb] 11:[tc] 11:[il] 11:[thv] 11:[pgc]
+11:[tl] 11:[ij] 11:[tg] 11:[qfx] 11:[tl] 11:[ij] 11:[tgc] 11:[8hv] 11:[wx] 11:[tl] 11:[wfx] 11:[usl] 11:[wh] 11:[tf] 11:[ws] 11:[8f] 11:[wh] 11:[tsl] 11:[wh] 11:[9PJ] 11:[Eg] 11:[yd] 11:[Esl] 11:[idz] 11:[EJ] 11:[yg] 7:[fx] 3:E
+3:[dz] 7:[9sl] 11:[Eg] 11:[yd] 11:[EPJ] 11:[*pj] 11:[eS] 11:[Tf] 11:[ep] 11:[uj] 11:[eS] 11:[Tf] 11:[ej] 11:[*p] 11:[eS] 11:[Tf] 11:[ep] 11:[9g] 5:j 5:[Ez] 5:c 5:[yz] 5:j 5:[Ef] 5:j 5:[id] 5:j 5:[Ez]
+5:b 5:[yz] 5:b 5:[Ez] 5:j 5:[9S] 5:g 5:[Ej] 5:L 5:[yj] 5:g 5:[Ed] 11:[*f] 5:j 5:[ex] 5:L 5:[Tj] 5:f 5:[eS] 5:j 5:[up] 5:f 5:[ej] 5:x 5:[Tb] 5:x 5:[eb] 5:x
+5:[*b] 5:x 5:[ej] 5:f 5:[Tp] 11:e 11:[9g] 5:j 5:[Ez] 5:c 5:[yz] 5:j 5:[Ef] 5:j 5:[id] 5:j 5:[Ez] 5:b 5:[yz] 5:b 5:[Ez] 5:j 5:[9S] 5:g 5:[Ej] 5:L 5:[yj] 5:g
+5:[Ed] 11:[8f] 5:h 5:[wl] 5:x 5:[tv] 5:m 5:[wx] 5:v 5:[um] 5:v 5:[wx] 5:m 5:[tv] 5:x 5:[wl] 5:h 5:[0s] 5:f 5:[th] 5:l 5:[ox] 5:v 5:[tc] 5:l 5:[qj] 5:l 5:[tc]
+5:b 5:[ic] 5:l 5:[th] 5:l 5:[pg] 5:l 5:[tc] 5:b 5:[iz] 5:b 5:[tc] 5:l 5:[qf] 5:j 5:[tl] 5:x 5:[il] 5:j 5:[tg] 11:[8h] 5:l 5:[wv] 5:x 5:[tl] 5:h 5:[wf] 5:l
+5:[us] 5:h 5:[wl] 5:v 5:[tm] 5:v 5:[wm] 5:v 5:[8m] 5:v 5:[wl] 5:h 5:[ts] 11:w 11:[9P] 5:d 5:[Eg] 5:J 5:[yg] 5:d 5:[Es] 5:g 5:[id] 5:g 5:[EJ] 5:z 5:[yc] 5:z
+5:[EJ] 5:g 5:[9s] 5:g 5:[EJ] 5:l 5:[yJ] 5:g 5:[EP] 11:[*p] 5:S 5:[ef] 5:j 5:[TL] 5:x 5:[eb] 5:L 5:[ub] 5:L 5:[ex] 5:b 5:[TL] 5:x 5:[eb] 5:L 5:[6x] 5:j 5:[0L]
+5:f 5:[Tj] 5:S 5:[0f] 11:[6upS] 11:[6upS] 11:[6upS] 11:[6upS] 11:[6upad] 11:[6upad] 11:[6upad] 11:[6upad] 11:[6upSf] 11:[6upSf] 11:[6upSf] 11:[6upSf] 11:[ypj] 5:h 5:[eG] 5:d 5:[9p] 5:o 5:[2I] 5:y 5:[2e] 5:w 5:Q 5:9
+5:6 5:9 5:Q 5:w 5:[9e] 5:y 5:[eI] 5:o 5:[yp] 5:d 5:[IG] 5:j 5:[oak] 7:[yh] 7:[rd] 7:[wa] 7:[rd] 7:[yh] 7:[Ipj] 7:[yh] 7:[rd] 7:[wp] 7:[rd] 7:[yh] 7:[uoh] 7:[yd] 7:[ra] 7:[wo]
+7:a 7:d 7:[ypj] 5:h 5:[eG] 5:d 5:[9p] 5:o 5:[2I] 5:y 5:[2e] 5:w 5:Q 5:9 5:6 5:9 5:Q 5:w 5:[9e] 5:y 5:[eI] 5:o 5:[yp] 5:d 5:[IG] 5:j 5:[oak] 7:[yh]
+7:[rd] 7:[wa] 7:[rd] 7:[yh] 7:[Ipj] 7:[yh] 7:[rd] 7:[wp] 7:[rd] 7:[yh] 7:[uoh] 7:[yd] 7:[ra] 7:[wo] 7:a 7:d 7:[ypj] 5:h 5:[eG] 5:d 5:[9p] 5:o 5:[2I] 5:y 5:[2e] 5:w 5:Q 5:9
+5:6 5:9 5:Q 5:w 5:[9e] 5:y 5:[6I] 5:o 5:[2p] 5:d 5:G 5:j 5:[5ak] 7:h 7:[9d] 7:a 7:[wd] 7:h 7:[eSL] 7:k 7:[9h] 7:S 7:[5h] 7:k 7:[rdz] 7:k 7:[9h] 7:d
+7:[5a] 7:o 7:[2IpdG] 15:6 15:[9e] 7:y 7:[QI] 7:p 7:[ed] 7:G 7:[yj] 7:z 7:[IC] 7:z 7:[yj] 7:G 7:[ed] 7:p 7:[6upSf] 15:[6upSf] 15:[6upSf] 15:[6IpSG] 15:[6IpSG] 15:[6IpSG] 15:[6oSfh] 15:[6oSfh] 15:[6oSfh] 15:[2pdGj]
+11:6 11:[9e] 5:y 5:[QI] 5:p 5:[ed] 5:G 5:[yj] 5:z 5:[IC] 5:z 5:[yj] 5:G 5:[ez] 5:j 5:[QG] 5:d 5:[9j] 5:G 5:[6d] 5:p 5:[5ak] 7:h 7:d 7:[wa] 7:[rd] 7:[yh] 7:[Ipj]
+7:[yh] 7:[rd] 7:[wp] 7:[rd] 7:[yh] 7:[uoh] 7:[yd] 7:[ra] 7:[wo] 7:a 7:d 7:[2pdGj] 11:6 11:[9e] 5:y 5:[QI] 5:p 5:[ed] 5:G 5:[yj] 5:z 5:[IC] 5:z 5:[yj] 5:G 5:[ez] 5:j 5:[QG]
+5:d 5:[9j] 5:G 5:[6d] 5:p 5:[5adhk] 23:w 11:y 11:[IpdGj] 11:y 11:w 11:y 11:[uoafh] 11:y 11:w 23:[2pdGj] 11:6 11:[9e] 5:y 5:[QI] 5:p 5:[ed] 5:G 5:[yj] 5:z 5:[IC] 5:v 5:[yC]
+5:z 5:[ej] 5:h 5:[QG] 5:d 5:[9p] 11:6 11:[5adhk] 23:w 11:y 11:[pShjL] 11:y 11:w 11:y 11:[adhkz] 11:y 11:w 23:[6IpdG] 23:[jC] 7:[Gz] 7:[dj] 7:[Gz] 7:[dj] 7:[pG] 7:[dj] 7:[pG] 7:[Id] 7:[pG]
+7:[Id] 7:[yp] 7:[Id] 7:[yp] 7:[eI] 7:[6upSf] 15:6 7:0 7:e 7:u 7:p 7:f 7:j 7:x 7:j 7:x 7:b 23:j 23:[ypdgjc] 35:x 11:z 47:L 35:z 11:[TpSfjx] 35:L 11:j 71:j 23:[ypdgjc]
+35:x 11:z 47:L 35:z 11:[tosfhx] 95:[usfhl] 47:[isgjlb] 35:v 11:c 47:x 35:c 11:[usfhlv] 35:x 11:l 83:l 11:[yPdgJ] 35:l 11:z 47:l 35:J 11:[TpSfj] 119:[pSfj] 23:[2gjzc] 11:6 11:9 11:[0fjzx] 11:[qdgjz] 11:e
+11:y 11:e 11:[TSgjL] 11:e 11:q 11:[ydgjz] 11:[6fjLx] 11:0 11:e 11:[TSfjL] 11:[upSfj] 11:T 11:e 11:0 11:6 11:5 11:[4pSfj] 11:3 11:[2gjzc] 11:6 11:9 11:[0fjzx] 11:[qdgjz] 11:e 11:y 11:e 11:[TSgjL] 11:e
+11:q 11:[ydgjz] 11:[1fhlx] 11:5 11:8 11:9 11:0 11:w 11:t 11:y 11:[usfhl] 11:o 11:s 23:[4jlcb] 11:8 11:q 11:[whlcv] 11:[egjlc] 11:t 11:i 11:t 11:[ufjlx] 11:t 11:e 11:[igjlc] 11:[1hlxv] 11:5 11:8
+11:[9fhlx] 11:[0sfhl] 11:w 11:t 11:y 11:u 11:y 11:t 11:[wsfhl] 11:[9PdgJ] 11:E 11:y 11:[EsgJl] 11:[idgJz] 11:E 11:y 7:[fx] 3:E 3:[dz] 7:[^sgJl] 11:E 11:y 11:[EPdgJ] 11:[*pfj] 11:e 11:T 11:e 11:u
+11:e 11:T 11:e 11:p 11:T 11:[uj] 23:[yJ] 11:P 11:d 11:[Pl] 11:[gz] 11:P 11:d 11:[Px] 5:z 5:[yl] 11:P 11:d 11:[PJ] 11:[Tj] 11:p 11:S 11:p 11:f 11:S 11:p 11:u 11:T
+11:e 11:[0pj] 11:6 11:[^PdgJ] 11:4 11:^ 11:[9sgJl] 11:[qdgJz] 11:E 11:y 7:[fx] 3:i 3:[dz] 7:[PsgJl] 35:[PdgJ] 11:[6pSfj] 11:3 11:6 11:0 11:e 11:u 11:p 11:u 11:[epSfj] 11:0 11:6 11:3 11:[2pdGj]
+11:6 11:9 11:0 12:Q 11:e 12:y 12:u 12:I 12:p 12:d 13:f 13:G 12:j 14:z 13:x 14:C 13:b 15:z 14:x 14:C 31:b
+]=]
+local TRAILING_RESTS = 47
+
+local sheet = {}
+for encoded in DATA:gmatch("%S+") do
+    local gap, token = encoded:match("^(%d+):(.+)$")
+    gap = tonumber(gap)
+    if gap and token then
+        for _ = 1, gap do
+            sheet[#sheet + 1] = "-"
+        end
+        sheet[#sheet + 1] = token
+    end
+end
+for _ = 1, TRAILING_RESTS do
+    sheet[#sheet + 1] = "-"
+end
+
+return {
+    Id="conquest-of-paradise-vangelis",
+    Name="Conquest of Paradise",
+    Artist="Vangelis",
+    Arranger="Patrik Pietschmann",
+    BPM=70,
+    StepsPerBeat=48,
+    Complete=true,
+    DurationSeconds=283.985,
+    OriginalDurationSeconds=283.985,
+    TempoEvents=12,
+    TimeSignatures={"3/4"},
+    Source="User-supplied MIDI file",
+    SourceFile="conquest-of-paradise-vangelis-arrangement-by-patrik-pietschmann.mid",
+    SourceLicense="User-provided source file; converted for Velora at the repository owner's request.",
+    Categories={"Famous","Soundtrack","Film","Vangelis","Cinematic","Epic","Piano","Patrik Pietschmann","Complete"},
+    Notes=table.concat(sheet, " "),
+}
