269f4f1089e076a9216f55292645d25ad835ac2a
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-20 02:58:59 +0000

    Register Golden Brown x Love Story in Velora

--- runtime.lua
+++ runtime.lua
@@ -106,7 +106,7 @@ releaseSource = replaceOnce(
 releaseSource = replaceOnce(
     releaseSource,
     'AssetRevision = "0.10.21-seekfix1"',
-    'AssetRevision = "0.10.34-dishes-1"',
+    'AssetRevision = "0.10.35-golden-love-1"',
     "song library revision"
 )
 
