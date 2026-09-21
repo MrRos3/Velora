63dc931a1deeafcb5ecc06aa151ac8ee999c1098
MrRos3 <114161345+MrRos3@users.noreply.github.com>
2026-09-22 01:51:57 +0400

    Add Next to You Parasyte piano arrangement

---
+++ songs/NextToYou.lua
@@ -0,0 +1,70 @@
+-- Velora 0.10.38 Nova — complete arrangement converted from a MIDI supplied by the repository owner.
+-- Source file: parasyte-ost-next-to-you-theishter-arrange.mid
+-- Source SHA-256: 5FD9A96092E62860832B64C85B8A4D3E4F07D1AA0D3683D6824C01DEB3AE691C
+-- MIDI: 480 PPQ, 1 source tempo event(s), source time signature(s): 4/4, key signature(s): G, Ab.
+-- 2 piano tracks retained; simultaneous duplicate pitches are merged.
+-- 32 out-of-range note-on event(s) were octave-folded into Velora's C2-C7 keyboard range.
+-- 1,586 source note-ons became 1,558 mapped notes after same-step duplicate merging.
+-- Timing normalized to real seconds at 120 BPM / 48 Velora steps per beat.
+-- Arrangement: Theishter.
+
+local DATA = [=[
+0:[uh] 23:[af] 23:[oh] 23:[ak] 23:[tsfj] 23:o 23:u 23:[oh] 11:G 11:[yh] 23:p 23:I 23:[pj] 11:h 11:[yG] 23:[pf] 23:[rd] 23:[IG] 23:[ufh] 23:[af] 23:[oh] 23:[ak] 23:[tj] 23:o 23:u 23:[oh] 11:G 11:[yG] 23:p 23:[Id] 23:[pj] 11:h
+11:[yj] 23:[pl] 23:[rk] 23:[yG] 23:[uh] 23:[af] 23:[oh] 23:[az] 23:[tj] 23:o 23:u 23:[oh] 11:G 11:[yG] 23:p 23:I 23:[pj] 11:h 11:[oz] 23:a 23:[Ik] 23:[aj] 23:[ufh] 23:a 23:[of] 23:[ak] 23:[tsfj] 23:o 23:u 23:[oh] 11:G 11:[yG]
+23:p 23:I 23:[pj] 11:h 11:[yG] 23:[pl] 23:[rk] 23:[IG] 23:[3ofh] 23:[0uof] 11:r 11:[uofh] 11:r 11:[0ak] 23:[18psj] 23:8 11:w 11:[tu] 23:[woh] 11:[IG] 11:[29oh] 23:9 11:Q 11:[ey] 23:[Qpj] 11:[oh] 11:[29IG] 23:[9Qeuf] 23:[7yId] 23:[79QrIG] 23:[30oah] 23:[0uf]
+11:w 11:[roah] 23:[ak] 23:[18psfj] 23:8 11:w 11:[tu] 23:[woh] 11:[IG] 11:[29IG] 23:9 11:Q 11:[ey] 23:[Qpj] 11:[oh] 11:[29pj] 23:[9Qesl] 23:[7ak] 23:[79QrIG] 23:[30oah] 23:[0uf] 11:w 11:[roah] 23:[dz] 23:[18psfj] 23:8 11:w 11:[tu] 23:[woh] 11:[IG] 11:[29IG] 23:9
+11:Q 11:[ey] 23:[Qpj] 11:[oh] 11:[5wdz] 23:[wo] 23:[$Qak] 23:[QIpj] 23:[30oafh] 23:0 11:w 11:[ruG] 23:[ak] 23:[18psfj] 23:8 11:w 11:[ru] 23:[eosh] 11:[IG] 11:[29IG] 23:9 11:Q 11:[ey] 23:[Qpj] 11:[oh] 11:[29IG] 23:[9sl] 23:[Qeyak] 23:[9IG] 23:[0uoah] 47:[uo] 47:[uo]
+47:[9yI] 47:[8wtu] 47:[tu] 23:w 23:[tu] 23:w 23:[8I] 23:[wo] 23:[9eyIp] 23:Q 23:[yIp] 23:Q 23:[ya] 23:Q 23:[ys] 23:Q 23:[5y] 5:[9o] 6:[wa] 34:[wyoa] 47:[$y] 5:[9I] 6:[Qp] 34:[yIp] 23:[eo] 23:[30uo] 47:[0ruo] 23:w 23:[ruo] 23:w 23:[9p] 47:[18tu]
+47:[8wus] 23:0 23:[wa] 23:0 23:[wto] 23:[0p] 23:[29yId] 47:[9eyIp] 23:Q 23:[9eyI] 47:[29ts] 47:[3ra] 23:3 11:[70w] 35:[70w] 23:[7ryp] 20:y 5:I 4:p 3:$ 0:d 5:G 4:[79Qj] 5:G 4:d 4:p 5:I 1:$ 2:y 20:[18sfh] 23:[8u] 5:o
+5:s 5:f 5:[wts] 5:o 5:s 5:f 5:[9h] 5:l 5:x 5:v 5:[Qyx] 5:l 5:h 5:f 5:[7s] 5:o 5:s 5:f 5:[Qrh] 5:f 5:s 5:o 5:[7u] 5:t 5:u 5:o 5:[18pdj] 23:[8y] 5:I 5:p 5:d 5:[QyG]
+5:k 5:d 5:p 5:[8d] 5:G 5:j 5:z 5:[QyC] 5:b 5:z 5:j 5:[8d] 5:p 5:d 5:G 5:[Qyj] 5:d 5:p 5:I 5:[8y] 5:e 5:y 5:I 5:[7pdj] 23:[7y] 5:I 5:p 5:d 5:[QrG] 5:k 5:d 5:p
+5:[7d] 5:G 5:j 5:z 5:[QyC] 5:b 5:z 5:j 5:[7d] 5:p 5:d 5:G 5:[Qrj] 5:d 5:p 5:I 5:[7y] 5:e 5:y 5:I 5:[30oah] 23:[0o] 5:a 5:f 5:h 5:[wruk] 5:x 5:k 5:h 5:[0f] 5:a 5:o
+5:a 5:[29IaG] 23:[9a] 5:d 5:G 5:k 5:[Qryz] 5:C 5:n 5:C 5:[9z] 5:k 5:G 5:d 5:[18uof] 23:[8u] 5:o 5:s 5:d 5:[wth] 5:l 5:h 5:f 5:[8h] 5:l 5:x 5:v 5:[wum] 5:x 5:m 5:v 5:[8x]
+5:v 5:x 5:v 5:[wtm] 5:v 5:x 5:l 5:[8h] 5:f 5:s 5:o 5:[6uG] 23:[6u] 5:o 5:s 5:f 5:[0eh] 5:l 5:h 5:f 5:[6h] 5:l 5:x 5:v 5:[0tm] 5:x 5:m 5:v 5:[6x] 5:l 5:h 5:f
+5:[0eh] 5:l 5:h 5:f 5:[6s] 5:o 5:u 5:o 5:[7uIaf] 23:[7u] 5:I 5:a 5:f 5:[QrG] 5:k 5:x 5:G 5:[7k] 5:G 5:f 5:G 5:[Quk] 5:x 5:n 5:x 5:[7n] 5:x 5:k 5:G 5:[Qrf] 5:G 5:j
+5:G 5:[7f] 5:a 5:I 5:u 5:[7IaG] 23:[7r] 5:Y 5:I 5:a 5:[(QrD] 5:G 5:k 5:Z 5:[7C] 5:n 5:Z 5:n 5:[@(C] 5:Z 5:n 5:C 5:[(Z] 5:k 5:G 5:D 5:[QrYa] 5:D 5:G 5:k 5:[(G] 5:a
+5:D 5:G 5:[3ofh] 23:[0uof] 11:r 11:[uofh] 11:r 11:[0ak] 23:[18psj] 23:8 11:w 11:[tu] 23:[woh] 11:[IG] 11:[29oh] 23:9 11:Q 11:[ey] 23:[Qpj] 11:[oh] 11:[29IG] 23:[9Qeuf] 23:[7yId] 23:[79QrIG] 23:[3oah] 23:[0uf] 11:r 11:[uoah] 11:r 11:[0ak] 23:[18psfj] 23:8
+11:w 11:[tu] 23:[woh] 11:[IG] 11:[29IG] 23:9 11:Q 11:[ey] 23:[Qpj] 11:[oh] 11:[29pj] 23:[9Qesl] 23:[7ak] 23:[79QrIG] 23:[30oah] 23:[0uf] 11:r 11:[uoah] 11:r 11:[0dz] 23:[18psfj] 23:8 11:w 11:[tu] 23:[woh] 11:[IG] 11:[29IG] 23:9 11:Q 11:[ey] 23:[Qpj] 11:[oh]
+11:[5wdz] 23:[wo] 23:[$Qak] 23:[QIpj] 23:[30oafh] 23:0 11:r 11:[uG] 11:r 11:[0ak] 23:[18psfj] 23:8 11:w 11:[tu] 23:[eosh] 11:[IG] 11:[29IG] 23:9 11:Q 11:[ey] 23:[Qpj] 11:[oh] 11:[29IG] 23:[9sl] 23:[Qeyak] 23:[9IG] 23:[4OgH] 23:[qiOg] 11:t 11:[iOgH] 11:t 11:[qsl]
+23:[!PSJ] 23:* 11:W 11:T 11:W 11:[(OH] 11:[oh] 11:[@OH] 23:( 11:E 11:Y 11:E 11:[(PJ] 11:[^OH] 11:[@(oh] 23:[(wEig] 23:[18YoD] 23:[(wtoh] 23:[4OsH] 23:[qig] 11:t 11:[iOsH] 11:t 11:[qsl] 23:[!PSgJ] 23:* 11:W 11:T 11:W 11:[*OH] 11:[oh] 11:[@oh]
+23:( 11:E 11:Y 11:E 11:[(PJ] 11:[^OH] 11:[@(PJ] 23:[(wESL] 23:[18sl] 23:[(wtoh] 23:[4OsH] 23:[qig] 11:t 11:[iOsH] 11:t 11:[qDZ] 23:[!PSgJ] 23:* 11:W 11:T 11:W 11:[*OH] 11:[oh] 11:[@oh] 23:( 11:E 11:Y 11:E 11:[(PJ] 11:[^OH] 11:[%DJZ] 23:[%W]
+23:[5sDHl] 23:[5wPJ] 23:[4OsgH] 23:q 11:t 11:[ig] 11:t 11:[qsl] 23:[!PSgJ] 23:* 11:W 11:T 11:W 11:[*OSH] 11:[oh] 11:[@oh] 23:( 11:E 11:Y 11:E 11:[(PJ] 11:[^OH] 11:[@(oh] 23:[(wESL] 23:[(wEYsl] 23:[(woh] 23:[qtiOsH] 23:[WiOg] 23:[tOgH] 23:[Wsl] 23:[*PSJ] 23:W
+23:[Ti] 23:[WOH] 11:[oh] 11:[(OH] 23:E 23:[Yo] 23:[EPJ] 11:[OH] 11:[(oh] 23:[Eig] 23:[8tYoD] 23:[woh] 23:[qiOsH] 23:[tig] 23:[WOsH] 23:[tsl] 23:[*PSgJ] 23:W 23:[Ti] 23:[WOH] 11:[oh] 11:[(oh] 23:E 23:[Yo] 23:[EPJ] 11:[OH] 11:[(PJ] 23:[ESL] 23:[%s] 3:[8D] 3:[(h] 4:[Yl]
+10:[woh] 23:[qtiOsH] 23:[Wig] 23:[tOsH] 23:[WDZ] 23:[*PSgJ] 23:W 23:[Ti] 23:[WOH] 11:[oh] 11:[(oh] 23:E 23:[Yo] 23:[EPJ] 11:[OH] 11:[WDJZ] 23:Y 11:w 11:[YsDHl] 23:[YPJ] 23:[qtiOsgH] 23:W 23:[tig] 23:[Wsl] 23:[*PSgJ] 23:W 23:[Ti] 23:[WOSH] 11:[oh] 11:[(oh] 23:E 23:[Yo]
+23:[EPJ] 11:[OH] 11:[(oh] 23:[ESL] 23:[Yosl] 23:[Eoh] 23:^ 5:* 6:q 5:E 4:[OsDH] 1:T
+]=]
+local TRAILING_RESTS = 165
+
+local sheet = {}
+for encoded in DATA:gmatch("%S+") do
+    local gap, token = encoded:match("^(%d+):(.+)$")
+    gap = tonumber(gap)
+    if gap and token then
+        for _ = 1, gap do
+            sheet[#sheet + 1] = "-"
+        end
+        sheet[#sheet + 1] = token
+    end
+end
+for _ = 1, TRAILING_RESTS do
+    sheet[#sheet + 1] = "-"
+end
+
+return {
+    Id="next-to-you-parasyte-theishter",
+    Name="Next to You",
+    Artist="Ken Arai",
+    BPM=120,
+    StepsPerBeat=48,
+    Complete=true,
+    DurationSeconds=114.001,
+    OriginalDurationSeconds=114.001,
+    TempoEvents=1,
+    TimeSignatures={"4/4"},
+    KeySignatures={"G","Ab"},
+    Source="User-supplied MIDI file",
+    SourceFile="parasyte-ost-next-to-you-theishter-arrange.mid",
+    SourceLicense="User-provided source file; converted for Velora at the repository owner's request.",
+    Categories={"Famous","Anime","Parasyte","Soundtrack","Ken Arai","Theishter","Piano","Emotional","Complete"},
+    Notes=table.concat(sheet, " "),
+}
